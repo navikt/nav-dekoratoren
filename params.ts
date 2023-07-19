c1bba5d26fa2643dcaa3c265a6130831d5e8f85f
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-07-19 12:23:38 +0200

    Completed params schema

--- params.ts
+++ params.ts
@@ -1,9 +1,49 @@
 import z from "zod";
 
+const authLevelSchema = z.enum(["Level3", "Level4"]);
+// export
+// nb / nn / en / se / pl / uk / ru
+const languageSchema = z.enum(["nb", "nn", "en", "se", "pl", "uk", "ru"]);
+const contextSchema = z.enum([
+  "privatperson",
+  "arbeidsgiver",
+  "samarbeidspartner",
+]);
+
+const breadcrumbSchema = z.object({
+  title: z.string(),
+  url: z.string().url(),
+  handleInApp: z.boolean().default(false),
+});
+
+const background = z.enum(["white", "gray", "transparent"]);
+
 const paramsSchema = z.object({
-  context: z
-    .enum(["privatperson", "arbeidsgiver", "samarbeidspartner"])
-    .default("privatperson"),
+  context: contextSchema.default("privatperson"),
+  simple: z.boolean().default(false),
+  simpleHeader: z.boolean().default(false),
+  simpleFooter: z.boolean().default(false),
+  enforceLogin: z.boolean().default(false),
+  redirectToApp: z.boolean().default(false),
+  level: authLevelSchema.default("Level3"),
+  language: languageSchema.default("nb"),
+  availableLanguages: z
+    .array(
+      z.object({
+        locale: languageSchema,
+        url: z.string().url(),
+      })
+    )
+    .default([]),
+  breadcrumbs: z.array(breadcrumbSchema).default([]),
+  utilsBackground: background.default("transparent"),
+  feedback: z.boolean().default(false),
+  chatbot: z.boolean().default(true),
+  chatbotVisible: z.boolean().default(false),
+  urlLookupTable: z.boolean().default(false),
+  shareScreen: z.boolean().default(false),
+  logoutUrl: z.string().url().optional(),
+  maskHotjar: z.boolean().default(false),
 });
 
 export function parseParams(params: any) {
