5e5f7a4c907765fc2b797f769c9443df7adf71ce
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-07-26 21:29:26 +0200

    Breadcrumbs test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.IS_OLD ? "https://nav.no" : "http://localhost:3001",
+    baseUrl: process.env.IS_OLD
+      ? "https://nav.no/dekoratoren"
+      : "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
-  })
-})
+describe("template spec", () => {
+  it("Breadcrumbs is set", () => {
+    const breadcrumbs = [
+      {
+        url: "https://www.nav.no/person/dittnav",
+        title: "Ditt NAV",
+      },
+      {
+        url: "https://www.nav.no/person/kontakt-oss",
+        title: "Kontakt oss",
+      },
+    ];
+    cy.visit(
+      "/?breadcrumbs=" + encodeURIComponent(JSON.stringify(breadcrumbs))
+    );
+
+    cy.get("#breadcrumbs-list").should("exist");
+    // Verify that the breadcrumbs are rendered
+    cy.get("#breadcrumbs-list").should("contain", "Ditt NAV");
+    cy.get("#breadcrumbs-list").should("contain", "Kontakt oss");
+  });
+});
