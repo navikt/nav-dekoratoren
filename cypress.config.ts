2184b54e78282573bb87e455a96be888bb807a19
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-07-27 15:21:21 +0200

    Removed screenshots folder

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
       video: false,
+      screenshotOnRunFailure: false,
+      screenshotsFolder: false,
     baseUrl: process.env.IS_OLD
       ? "https://nav.no/dekoratoren"
       : "http://localhost:3000",
