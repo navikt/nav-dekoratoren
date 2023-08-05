70ef5cec3e3178e8ba0aee4f237a65e38fc9e043
Maximilian Øystå Lloyd <me@maxlloyd.no>
2023-08-05 16:33:45 +0200

    Litt refactor og benchmarking setup

--- server.ts
+++ server.ts
@@ -13,145 +13,148 @@ const isProd = process.env.NODE_ENV === 'production';
 const port = 3000;
 const host = process.env.HOST ?? `http://localhost:${port}`;
 
-const app = express();
+const entryPointPath = 'client/main.ts';
 
-app.use(cors());
-app.use(express.static(isProd ? 'dist' : 'public'));
-app.use(decoratorParams);
+const script = (src: string) => `<script type="module" src="${src}"></script>`;
 
-app.use('/dekoratoren/api/sok', async (req: Request<{ ord: string }>, res) => {
-  res.send(
-    await fetch(
-      `https://www.nav.no/dekoratoren/api/sok?ord=${req.params.ord}`,
-    ).then((res) => res.json()),
-  );
-});
-
-app.use('/footer', async (req, res) => {
-  const params = req.decorator;
-  // Maybe make into middleware
-  const data = await getData(params);
-
-  return res.status(200).send(
-    Footer({
-      simple: req.decorator.simple,
-      personvern: data.personvern,
-      footerLinks: data.footerLinks,
-      feedback: req.decorator.feedback,
-      texts: data.texts,
-    }),
-  );
-});
-
-app.use('/header', async (req, res) => {
-  const params = req.decorator;
-  const data = await getData(params);
-  return res.status(200).send(
-    HeaderMenuLinks({
-      headerMenuLinks: data.headerMenuLinks,
-    }),
-  );
-});
-
-app.get('/data/:key', async (req, res) => {
-  const { params } = req;
-  const dataKey = params.key as DataKeys;
-
-  if (!dataKey) {
-    return res.status(400).send('Missing key');
-  }
-
-  const data = await getData(req.decorator);
-  const subset = data[dataKey];
-
-  if (!subset) {
-    res.status(404).send('Data not found with key:' + dataKey);
-  }
-
-  res.send(subset);
-});
-
-app.use('/', async (req, res) => {
-  const entryPointPath = 'client/main.ts';
-
-  const getResources = async () =>
-    (
-      (await import('./dist/manifest.json', { assert: { type: 'json' } }))
-        .default as {
-        [entryPointPath]: { file: string; css: string[] };
-      }
-    )[entryPointPath];
-
-  const scripts = async () => {
-    const script = (src: string) =>
-      `<script type="module" src="${src}"></script>`;
-
-    if (isProd) {
-      const resources = await getResources();
-      return script(`${host}/${resources.file}`);
-    } else {
-      return [
-        'http://localhost:5173/@vite/client',
-        `http://localhost:5173/${entryPointPath}`,
-      ]
-        .map(script)
-        .join('');
+const getResources = async () => {
+  const resources = (
+    (await import('./dist/manifest.json', { assert: { type: 'json' } }))
+      .default as {
+      [entryPointPath]: { file: string; css: string[] };
     }
-  };
-  const links = async () => {
-    if (isProd) {
-      const resources = await getResources();
-      return [
+  )[entryPointPath];
+
+  if (isProd) {
+    return {
+      scripts: script(`${host}/${resources.file}`),
+      styles: [
         ...resources.css.map(
           (href: string) =>
             `<link type="text/css" rel="stylesheet" href="${host}/${href}"></link>`,
         ),
-      ].join('');
-    } else {
-      return '';
-    }
+      ].join(''),
+    };
+  }
+
+  return {
+    styles: '',
+    scripts: [
+      'http://localhost:5173/@vite/client',
+      `http://localhost:5173/${entryPointPath}`,
+    ]
+      .map(script)
+      .join(''),
   };
+};
 
-  const data = await getData(req.decorator);
+async function bootstrap() {
+  const resources = await getResources();
+  const app = express();
 
-  res.status(200).send(
-    Index({
-      scripts: await scripts(),
-      links: await links(),
-      language: req.decorator.language,
-      header: Header({
-        texts: data.texts,
-        mainMenu: data.mainMenu,
-        headerMenuLinks: data.headerMenuLinks,
-        innlogget: false,
-        isNorwegian: true,
-        breadcrumbs: req.decorator.breadcrumbs,
-        utilsBackground: req.decorator.utilsBackground,
-        availableLanguages: req.decorator.availableLanguages,
-      }),
-      footer: Footer({
-        texts: data.texts,
+  app.use(cors());
+  app.use(express.static(isProd ? 'dist' : 'public'));
+  app.use(decoratorParams);
+
+  app.use(
+    '/dekoratoren/api/sok',
+    async (req: Request<{ ord: string }>, res) => {
+      res.send(
+        await fetch(
+          `https://www.nav.no/dekoratoren/api/sok?ord=${req.params.ord}`,
+        ).then((res) => res.json()),
+      );
+    },
+  );
+
+  app.use('/footer', async (req, res) => {
+    const params = req.decorator;
+    // Maybe make into middleware
+    const data = await getData(params);
+
+    return res.status(200).send(
+      Footer({
+        simple: req.decorator.simple,
         personvern: data.personvern,
         footerLinks: data.footerLinks,
-        simple: req.decorator.simple,
         feedback: req.decorator.feedback,
+        texts: data.texts,
       }),
-    }),
-  );
-});
+    );
+  });
 
-const server = http.createServer(app);
+  app.use('/header', async (req, res) => {
+    const params = req.decorator;
+    const data = await getData(params);
+    return res.status(200).send(
+      HeaderMenuLinks({
+        headerMenuLinks: data.headerMenuLinks,
+      }),
+    );
+  });
 
-if (!isProd) {
-  const wss = new WebSocketServer({ noServer: true });
+  app.get('/data/:key', async (req, res) => {
+    const { params } = req;
+    const dataKey = params.key as DataKeys;
 
-  server.on('upgrade', (request, socket, head) =>
-    wss.handleUpgrade(request, socket, head, (ws) =>
-      ws.emit('connection', ws, request),
-    ),
-  );
+    if (!dataKey) {
+      return res.status(400).send('Missing key');
+    }
+
+    const data = await getData(req.decorator);
+    const subset = data[dataKey];
+
+    if (!subset) {
+      res.status(404).send('Data not found with key:' + dataKey);
+    }
+
+    res.send(subset);
+  });
+
+  app.use('/', async (req, res) => {
+    const data = await getData(req.decorator);
+
+    res.status(200).send(
+      Index({
+        scripts: resources.scripts,
+        links: resources.styles,
+        language: req.decorator.language,
+        header: Header({
+          texts: data.texts,
+          mainMenu: data.mainMenu,
+          headerMenuLinks: data.headerMenuLinks,
+          innlogget: false,
+          isNorwegian: true,
+          breadcrumbs: req.decorator.breadcrumbs,
+          utilsBackground: req.decorator.utilsBackground,
+          availableLanguages: req.decorator.availableLanguages,
+        }),
+        footer: Footer({
+          texts: data.texts,
+          personvern: data.personvern,
+          footerLinks: data.footerLinks,
+          simple: req.decorator.simple,
+          feedback: req.decorator.feedback,
+        }),
+      }),
+    );
+  });
+
+  const server = http.createServer(app);
+
+  if (!isProd) {
+    const wss = new WebSocketServer({ noServer: true });
+
+    server.on('upgrade', (request, socket, head) =>
+      wss.handleUpgrade(request, socket, head, (ws) =>
+        ws.emit('connection', ws, request),
+      ),
+    );
+  }
+
+  server.listen(port, function () {
+    console.log(`Listening on http://localhost:${port}`);
+  });
 }
 
-server.listen(port, function () {
-  console.log(`Listening on http://localhost:${port}`);
-});
+bootstrap();
