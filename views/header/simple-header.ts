8dcd89c8c58376b69da52b555eede54cb4f01665
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-08-31 13:38:55 +0200

    Login button for simple header

--- views/header/simple-header.ts
+++ views/header/simple-header.ts
@@ -2,6 +2,8 @@ import type { AvailableLanguage, Breadcrumb, UtilsBackground } from '@/params';
 import { html } from '@/utils';
 import { Texts } from '@/texts';
 import { Breadcrumbs } from '@/views/breadcrumbs';
+import { IconButton } from '@/views/components/icon-button';
+import { LoginIcon } from '@/views/icons/login';
 import LanguageSelector from '@/views/language-selector';
 import { utilsBackgroundClasses } from '.';
 
@@ -9,6 +11,8 @@ export function SimpleHeader({
   availableLanguages,
   breadcrumbs,
   utilsBackground,
+  innlogget,
+  texts,
 }: {
   texts: Texts;
   innlogget: boolean;
@@ -18,22 +22,30 @@ export function SimpleHeader({
 }) {
   return html`
     <div id="header-withmenu">
-      <div
-        id="menu-background"
-      ></div>
-      <header
-        class="siteheader"
-      >
-        <div
-          class="hovedmeny-wrapper"
-        >
+      <div id="menu-background"></div>
+      <header class="siteheader">
+        <div class="hovedmeny-wrapper">
           <div class="hovedmeny-content">
             <img src="/ikoner/meny/nav-logo-black.svg" alt="NAV" />
           </div>
+          ${innlogget
+            ? html`<div>Innlogget</div>`
+            : html`
+                <div>
+                  ${IconButton({
+                    id: 'login-button',
+                    Icon: LoginIcon,
+                    text: texts.login,
+                  })}
+                </div>
+              `}
+        </div>
       </header>
-      <div class="decorator-utils-container ${
-        utilsBackgroundClasses[utilsBackground]
-      }">
+      <div
+        class="decorator-utils-container ${utilsBackgroundClasses[
+          utilsBackground
+        ]}"
+      >
         <!-- ${Breadcrumbs({ breadcrumbs })} -->
         ${LanguageSelector({ availableLanguages })}
       </div>
