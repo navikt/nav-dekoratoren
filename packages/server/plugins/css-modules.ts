807dc9906e05891e45f876b99b194e91feaeca0d
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-09-21 14:32:00 +0200

    Removed uneeded plugin code

--- packages/server/plugins/css-modules.ts
+++ packages/server/plugins/css-modules.ts
@@ -5,12 +5,6 @@ import postcss from 'postcss';
 plugin({
   name: 'css-modules',
   setup(build) {
-    const dir = './public/assets';
-    const fileName = `${dir}/styles.css`;
-
-    fs.mkdirSync(dir, { recursive: true });
-    fs.writeFileSync(fileName, '');
-
     build.onLoad({ filter: /\.module\.css$/ }, async ({ path }) => {
       const val = await postcss([
         // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -18,8 +12,6 @@ plugin({
           getJSON: () => {},
         }),
       ]).process(fs.readFileSync(path, 'utf8'), { from: path });
-
-      fs.appendFileSync(fileName, val.css);
 
       return {
         exports: {
