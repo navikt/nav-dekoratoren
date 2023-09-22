69c8735ed12c994252cea473985b83d45531e30a
Andreas Nordahl <andnorda@gmail.com>
2023-09-22 10:54:55 +0200

    return mock menu in dev

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -3,12 +3,18 @@ import { env } from './env/server';
 import ContentService from './content-service';
 import requestHandler from './request-handler';
 import SearchService from './search-service';
+import menu from './content-test-data.json';
 
 const server = Bun.serve({
   port: env.PORT || 8089,
   development: process.env.NODE_ENV === 'development',
   fetch: await requestHandler(
-    new ContentService(fetchMenu, fetchDriftsmeldinger),
+    new ContentService(
+      process.env.NODE_ENV === 'production'
+        ? fetchMenu
+        : () => Promise.resolve(menu),
+      fetchDriftsmeldinger,
+    ),
     new SearchService(fetchSearch),
   ),
 });
