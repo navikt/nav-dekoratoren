b5793f2f40afa9b4f40ccaa3cca808bfc234ec5d
Andreas Nordahl <andnorda@gmail.com>
2023-09-26 12:33:32 +0200

    removed unused varsler client views

--- packages/client/src/views/varsler.ts
+++ packages/client/src/views/varsler.ts
@@ -43,23 +43,6 @@ export async function fetchVarsler() {
   return varsler;
 }
 
-export function VarslerEmptyView({ texts }: { texts: Texts }) {
-  return html`
-    <div id="varsler-empty">
-      <img src="/public/kattIngenVarsler.svg" alt="Ingen varsler" />
-      <h1>${texts.varsler_tom_liste}</h1>
-      <p>${texts.varsler_tom_liste_ingress}</p>
-      <a href="${import.meta.env.VITE_MIN_SIDE_URL}/tidligere-varsler"
-        >${texts.varsler_vis_alle}</a
-      >
-    </div>
-  `;
-}
-
-export function VarslerUlest() {
-  return html` <div class="varsler-ulest"></div> `;
-}
-
 export function VarslerPopulated({
   texts,
   varslerData,
