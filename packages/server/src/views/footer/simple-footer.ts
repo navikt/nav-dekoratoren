1c1571976f82847035797974e88f64d9ec2e805d
Andreas Nordahl <andnorda@gmail.com>
2023-09-27 13:27:14 +0200

    working css modules for server components

--- packages/server/src/views/footer/simple-footer.ts
+++ packages/server/src/views/footer/simple-footer.ts
@@ -1,15 +1,17 @@
 import { Link } from 'decorator-shared/types';
 import html from 'decorator-shared/html';
+import classes from 'decorator-client/src/styles/simple-footer.module.css';
+import './wat.css';
 
 export type SimpleFooterProps = { links: Link[] };
 
 export const SimpleFooter = ({ links }: SimpleFooterProps) => html`
-  <footer class="simple-footer">
-    <ul class="footer-link-list">
+  <footer class="${classes.simpleFooter}">
+    <ul class="${classes.footerLinkList}">
       ${links.map(
         ({ content, url }) => html`
           <li>
-            <a class="footer-link" href="${url}">${content}</a>
+            <a class="${classes.footerLink}" href="${url}">${content}</a>
           </li>
         `,
       )}
