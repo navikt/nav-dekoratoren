0a14e315980773f26c852b4203e828f6046b221a
Andreas Nordahl <andnorda@gmail.com>
2023-10-10 10:25:42 +0200

    empty states

--- packages/shared/views/header/decorator-utils-container/decorator-utils-container.stories.ts
+++ packages/shared/views/header/decorator-utils-container/decorator-utils-container.stories.ts
@@ -1,7 +1,7 @@
 import type { StoryObj, Meta } from '@storybook/html';
 import type { DecoratorUtilsContainerProps } from '.';
 import { DecoratorUtilsContainer } from '.';
-import { utilsBackgrounds } from '../../../params';
+import { AvailableLanguage, utilsBackgrounds } from '../../../params';
 
 const meta: Meta<DecoratorUtilsContainerProps> = {
   title: 'header/decorator-utils-container',
@@ -21,27 +21,54 @@ const meta: Meta<DecoratorUtilsContainerProps> = {
 export default meta;
 type Story = StoryObj<DecoratorUtilsContainerProps>;
 
+const breadcrumbs = [
+  {
+    title: 'Arbeid og opphold i Norge',
+    url: '/no/person/flere-tema/arbeid-og-opphold-i-norge',
+  },
+  {
+    title: 'Medlemskap i folketrygden',
+  },
+];
+
+const availableLanguages: AvailableLanguage[] = [
+  {
+    locale: 'nb',
+    url: 'https://www.nav.no/person/kontakt-oss',
+  },
+  {
+    locale: 'en',
+    url: 'https://www.nav.no/person/kontakt-oss/en/',
+  },
+];
+
 export const Default: Story = {
   args: {
     utilsBackground: 'gray',
-    breadcrumbs: [
-      {
-        title: 'Arbeid og opphold i Norge',
-        url: '/no/person/flere-tema/arbeid-og-opphold-i-norge',
-      },
-      {
-        title: 'Medlemskap i folketrygden',
-      },
-    ],
-    availableLanguages: [
-      {
-        locale: 'nb',
-        url: 'https://www.nav.no/person/kontakt-oss',
-      },
-      {
-        locale: 'en',
-        url: 'https://www.nav.no/person/kontakt-oss/en/',
-      },
-    ],
+    breadcrumbs,
+    availableLanguages,
+  },
+};
+
+export const NoAvailableLanguages: Story = {
+  args: {
+    utilsBackground: 'gray',
+    breadcrumbs,
+    availableLanguages: [],
+  },
+};
+
+export const NoBreadcrumbs: Story = {
+  args: {
+    utilsBackground: 'gray',
+    breadcrumbs: [],
+    availableLanguages,
+  },
+};
+export const Empty: Story = {
+  args: {
+    utilsBackground: 'gray',
+    breadcrumbs: [],
+    availableLanguages: [],
   },
 };
--- packages/shared/views/header/decorator-utils-container/index.ts
+++ packages/shared/views/header/decorator-utils-container/index.ts
@@ -15,18 +15,22 @@ export const DecoratorUtilsContainer = ({
   utilsBackground,
   breadcrumbs,
   availableLanguages,
-}: DecoratorUtilsContainerProps) => html`
-  <div
-    class="${clsx([
-      'decorator-utils-container',
-      utils.contentContainer,
-      cls.decoratorUtilsContainer,
-      {
-        [cls.white]: utilsBackground === 'white',
-        [cls.gray]: utilsBackground === 'gray',
-      },
-    ])}"
-  >
-    ${Breadcrumbs({ breadcrumbs })} ${LanguageSelector({ availableLanguages })}
-  </div>
-`;
+}: DecoratorUtilsContainerProps) =>
+  breadcrumbs.length > 0 || availableLanguages.length > 0
+    ? html`
+        <div
+          class="${clsx([
+            'decorator-utils-container',
+            utils.contentContainer,
+            cls.decoratorUtilsContainer,
+            {
+              [cls.white]: utilsBackground === 'white',
+              [cls.gray]: utilsBackground === 'gray',
+            },
+          ])}"
+        >
+          ${Breadcrumbs({ breadcrumbs })}
+          ${LanguageSelector({ availableLanguages })}
+        </div>
+      `
+    : null;
