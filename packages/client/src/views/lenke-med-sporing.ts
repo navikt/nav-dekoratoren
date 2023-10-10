9f23e9e3c1a8a1f93a9a4748a80b207486ccfc19
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-10-10 13:25:10 +0200

    Functional implementation of extends

--- packages/client/src/views/lenke-med-sporing.ts
+++ packages/client/src/views/lenke-med-sporing.ts
@@ -3,13 +3,13 @@ import classes from '../styles/lenke-med-sporing.module.css';
 import type { AnalyticsEventArgs } from '../analytics/constants';
 import clsx from 'clsx';
 
-export class LenkeMedSporingElement extends HTMLElement {
+export class LenkeMedSporingElement extends HTMLAnchorElement {
   constructor() {
     super();
 
-    const a = document.createElement('a');
-    a.innerHTML = this.getAttribute('data-children') || '';
-    a.href = this.getAttribute('href') || '';
+    // const a = document.createElement('a');
+    // a.innerHTML = this.getAttribute('data-children') || '';
+    // a.href = this.getAttribute('href') || '';
 
     const attrs = [
       'data-class-name',
@@ -25,10 +25,10 @@ export class LenkeMedSporingElement extends HTMLElement {
     ];
 
     const data = attrs.map((attr) => this.getAttribute(attr) || '');
-    const children = this.querySelector('#children')?.innerHTML || '';
+    // const children = this.querySelector('#children')?.innerHTML || '';
 
-    this.innerHTML = '';
-    a.innerHTML = children;
+    // this.innerHTML = '';
+    // a.innerHTML = children;
 
     const [
       className,
@@ -44,25 +44,24 @@ export class LenkeMedSporingElement extends HTMLElement {
       ? (JSON.parse(rawEventArgs) as AnalyticsEventArgs)
       : null;
 
-    a.setAttribute('role', this.getAttribute('role') || '');
-    a.setAttribute('id', this.getAttribute('id') || '');
-    a.setAttribute('tabindex', this.getAttribute('tabindex') || '');
-    a.setAttribute('lang', this.getAttribute('lang') || '');
-
     const extraAttrsVal = this.getAttribute('data-extra-attrs');
     const extraAttrs = extraAttrsVal
       ? (JSON.parse(extraAttrsVal) as [string, string][])
       : [];
 
     for (const [key, value] of extraAttrs) {
-      a.setAttribute(key, value);
+      this.setAttribute(key, value);
     }
 
-    a.className = clsx(classNameOverride || classes.dekoratorLenke, className, {
-      [classes.lenkeMedSporing]: defaultStyleEnabled,
-    });
+    this.className = clsx(
+      classNameOverride || classes.dekoratorLenke,
+      className,
+      {
+        [classes.lenkeMedSporing]: defaultStyleEnabled,
+      },
+    );
 
-    a.addEventListener('click', () => {
+    this.addEventListener('click', () => {
       if (eventArgs) {
         const payload = {
           ...eventArgs,
@@ -74,11 +73,13 @@ export class LenkeMedSporingElement extends HTMLElement {
       }
     });
 
-    this.appendChild(a);
+    // this.appendChild(a);
   }
 }
 
-customElements.define('lenke-med-sporing', LenkeMedSporingElement);
+customElements.define('lenke-med-sporing', LenkeMedSporingElement, {
+  extends: 'a',
+});
 
 /*
  * Definerer en helper funksjon for rendering siden det er mange paramtere som er viktig at typesjekkes
