a9c2a8be62511b19d9c325b73be011cc0f8e18cf
Andreas Nordahl <andnorda@gmail.com>
2023-10-11 08:34:56 +0200

    no global styles in feedback

--- packages/server/src/views/feedback.ts
+++ packages/server/src/views/feedback.ts
@@ -1,6 +1,5 @@
 import html from 'decorator-shared/html';
-
-import classes from 'decorator-client/src/styles/feedback.module.css';
+import cls from 'decorator-client/src/styles/feedback.module.css';
 import { Texts } from 'decorator-shared/types';
 import { Button } from 'decorator-shared/views/components/button';
 
@@ -8,32 +7,28 @@ export type FeedbackProps = {
   texts: Texts;
 };
 
-export function Feedback({ texts }: FeedbackProps) {
-  return html`
-    <div class="${classes.feedback}">
-      <div class="${classes.feedbackContent}">
-        <h2 class="${classes.feedbackTitle}">${texts.did_you_find}</h2>
-        <div class="mx-4">
-          <div class="button-wrapper small-gap">
-            ${Button({
-              text: texts.yes,
-              variant: 'outline',
-              wide: true,
-              bigLabel: true,
-              id: 'feedback-yes',
-              data: { answer: texts.yes },
-            })}
-            ${Button({
-              text: texts.no,
-              variant: 'outline',
-              wide: true,
-              bigLabel: true,
-              id: 'feedback-no',
-              data: { answer: texts.no },
-            })}
-          </div>
-        </div>
+export const Feedback = ({ texts }: FeedbackProps) => html`
+  <div class="${cls.feedback}">
+    <div class="${cls.feedbackContent}">
+      <h2 class="${cls.feedbackTitle}">${texts.did_you_find}</h2>
+      <div class="${cls.buttonWrapper}">
+        ${Button({
+          text: texts.yes,
+          variant: 'outline',
+          wide: true,
+          bigLabel: true,
+          id: 'feedback-yes',
+          data: { answer: texts.yes },
+        })}
+        ${Button({
+          text: texts.no,
+          variant: 'outline',
+          wide: true,
+          bigLabel: true,
+          id: 'feedback-no',
+          data: { answer: texts.no },
+        })}
       </div>
     </div>
-  `;
-}
+  </div>
+`;
