761b6f7f440006cc9ad868271708c2dc3d007559
Andreas Nordahl <andnorda@gmail.com>
2023-10-11 08:00:44 +0200

    add logout-warning.module.css

--- packages/server/src/views/logout-warning.ts
+++ packages/server/src/views/logout-warning.ts
@@ -1,16 +1,16 @@
 import html from 'decorator-shared/html';
-
+import cls from './logout-warning.module.css';
 import utilClasses from 'decorator-client/src/styles/utils.module.css';
 
 export type LogoutWarningProps = unknown;
 
 export function LogoutWarning() {
-  return html`<dialog class="modal" id="logout-warning">
-    <div class="modal-window">
-      <h1 id="logout-warning-title" class="modal-title">
+  return html`<dialog class="${cls.modal}" id="logout-warning">
+    <div class="${cls.modalWindow}">
+      <h1 id="logout-warning-title" class="${cls.modalTitle}">
         Du blir snart logget ut automatisk
       </h1>
-      <p id="logout-warning-body" class="modal-body">
+      <p id="logout-warning-body" class="${cls.modalBody}">
         Vil du fortsatt være innlogget?
       </p>
       <div class="button-wrapper">
