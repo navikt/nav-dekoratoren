cf2b04369e53e4a0123f3b26a1bd69ac2f57e03c
Andreas Nordahl <andnorda@gmail.com>
2023-11-03 13:44:15 +0100

    only send ClientTexts to browser

--- packages/client/src/listeners/index.ts
+++ packages/client/src/listeners/index.ts
@@ -1,9 +1,9 @@
-import { WithTexts } from 'decorator-shared/types';
+import { ClientTexts } from 'decorator-shared/types';
 import { addBreadcrumbEventListeners } from './breadcrumbs-listener';
 import { addFeedbackListener } from './feedback-listener';
 export { addBreadcrumbEventListeners } from './breadcrumbs-listener';
 
-export function onLoadListeners(params: WithTexts) {
+export function onLoadListeners(params: { texts: ClientTexts }) {
   addBreadcrumbEventListeners();
   addFeedbackListener(params);
 }
