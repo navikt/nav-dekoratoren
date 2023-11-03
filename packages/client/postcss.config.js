8121484d89a26c347ac12cad30087be2f3391aa6
Andreas Nordahl <andnorda@gmail.com>
2023-11-03 08:43:07 +0100

    remove unused purgecss

--- packages/client/postcss.config.js
+++ packages/client/postcss.config.js
@@ -1,23 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-useless-escape */
-/* eslint-disable @typescript-eslint/no-var-requires */
 const prefixer = require('postcss-prefix-selector');
-
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    'views/**/*.ts',
-    'views/*.ts',
-    'server.ts',
-    'params.ts',
-    'public/ikoner/**/*.svg',
-    'client/**/*.ts',
-  ],
-  css: ['client/main.css'],
-  defaultExtractor: (content) => {
-    return content.match(/[A-Za-z0-9_\-\[\.{1}?\]]+/g) || [];
-  },
-  variables: true,
-});
 
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
@@ -32,6 +13,5 @@ module.exports = {
         return selector;
       },
     }),
-    // purgecss,
   ],
 };
