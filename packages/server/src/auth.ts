f39f53d138fd3a79a340e2d49a5432cda9ab728e
Andreas Nordahl <andnorda@gmail.com>
2023-11-07 11:36:51 +0100

    new oauth lib

--- packages/server/src/auth.ts
+++ packages/server/src/auth.ts
@@ -1,20 +1,69 @@
-import { grantTokenXOboToken, isInvalidTokenSet } from "@navikt/next-auth-wonderwall"
+import * as oauth from 'oauth4webapi';
 
+const getIssuer = async () => {
+  const issuer = new URL(Bun.env.TOKEN_X_WELL_KNOWN_URL!);
+  const authServer = await oauth
+    .discoveryRequest(issuer)
+    .then((response) => oauth.processDiscoveryResponse(issuer, response));
+  return authServer;
+};
+
+const client: oauth.Client = {
+  client_id: process.env.TOKEN_X_CLIENT_ID!,
+  token_endpoint_auth_method: 'private_key_jwt',
+};
 
 // @TODO: Add access policy rules to tms-varsel-api
 export async function exchangeToken(request: Request) {
-    const accessToken = request.headers.get('Authorization')!.replace('Bearer ', '')!
+  const accessToken = request.headers
+    .get('Authorization')!
+    .replace('Bearer ', '')!;
 
-    const tokenX = await grantTokenXOboToken(accessToken, "dev-gcp:min-side:tms-varsel-api");
+  const parameters = new URLSearchParams();
+  parameters.set('audience', 'dev-gcp:min-side:tms-varsel-api');
+  parameters.set('subject_token', accessToken);
+  parameters.set('subject_token_type', 'urn:ietf:params:oauth:token-type:jwt');
+  /*
+  parameters.set(
+    'grant_type',
+    'urn:ietf:params:oauth:grant-type:token-exchange',
+  );
+  parameters.set(
+    'client_assertion_type',
+    'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+  );
+  */
 
-    if (isInvalidTokenSet(tokenX)) {
-        throw new Error(
-            `Unable to exchange token for tms-varsel-api token, reason: ${tokenX.message}`,
-            {
-                cause: tokenX.error,
-            },
-        )
+  const as = await getIssuer();
+
+  const response = await oauth.deviceAuthorizationRequest(
+    as,
+    client,
+    parameters,
+    {
+      clientPrivateKey: {
+        key: JSON.parse(Bun.env.TOKEN_X_PRIVATE_JWK!),
+      },
+    },
+  );
+
+  let challenges: oauth.WWWAuthenticateChallenge[] | undefined;
+  if ((challenges = oauth.parseWwwAuthenticateChallenges(response))) {
+    for (const challenge of challenges) {
+      console.log('challenge', challenge);
     }
+    throw new Error(); // Handle www-authenticate challenges as needed
+  }
 
-    return `Bearer ${tokenX}`
+  const result = await oauth.processClientCredentialsResponse(
+    as,
+    client,
+    response,
+  );
+  if (oauth.isOAuth2Error(result)) {
+    console.log('error', result);
+    throw new Error(); // Handle OAuth 2.0 response body error
+  } else {
+    return `Bearer ${result.access_token}`;
+  }
 }
