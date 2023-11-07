11c3814d0393481c8af3d9079338ec3daa4ed932
Andreas Nordahl <andnorda@gmail.com>
2023-11-07 07:38:45 +0100

    combine feedback and feedback success stories

--- packages/server/src/views/feedback.stories.ts
+++ packages/server/src/views/feedback.stories.ts
@@ -2,17 +2,25 @@ import type { StoryObj, Meta } from '@storybook/html';
 import type { FeedbackProps } from './feedback';
 import { Feedback } from './feedback';
 import { texts } from '../texts';
+import { FeedbackSuccess } from 'decorator-client/src/views/feedback-success';
 
 const meta: Meta<FeedbackProps> = {
   title: 'feedback',
   tags: ['autodocs'],
-  render: Feedback,
 };
 
 export default meta;
 type Story = StoryObj<FeedbackProps>;
 
 export const Default: Story = {
+  render: Feedback,
+  args: {
+    texts: texts.nb,
+  },
+};
+
+export const Success: Story = {
+  render: FeedbackSuccess,
   args: {
     texts: texts.nb,
   },
