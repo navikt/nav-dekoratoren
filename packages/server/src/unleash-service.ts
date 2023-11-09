872af85bdfb472b42a70f67cf75914d1037eb932
Terje Karlsen <terjeofnorway@gmail.com>
2023-11-09 12:33:11 +0100

    Update unleash-logic and wiring up to actuall Unleach Next instance (#132)
    Connecting to the proper Unleash Next-service and handling failed service sync as well as improving the default toggles when mocking.

--- packages/server/src/unleash-service.ts
+++ packages/server/src/unleash-service.ts
@@ -5,32 +5,32 @@ type Config = {
   mock?: boolean;
 };
 
-
 /**
  * @deprecated Only for testing deployment
  */
 export interface GetFeatures {
-    getFeatures(): Features;
+  getFeatures(): Features;
 }
 
 export default class UnleashService {
   unleashInstance: Unleash | null;
-  supportedFeatures: { [key: string]: boolean };
+  defaultToggles: { [key: string]: boolean };
   mock: boolean;
 
   constructor({ mock }: Config) {
     // @TODO: Here we should use server/env/schema
     const { UNLEASH_SERVER_API_TOKEN, UNLEASH_SERVER_API_URL } = process.env;
+
     // Important: If the Unleash Next service goes down, we don't want
     // screen sharing or the chatbot to automatically be disabled.
     // However, future toggles may want to default to false, so assign
     // this below:
-    this.supportedFeatures = {
+    this.defaultToggles = {
       'dekoratoren.skjermdeling': true,
       'dekoratoren.chatbotscript': true,
     };
     this.unleashInstance = null;
-    this.mock = mock || false;
+    this.mock = mock || process.env.ENV === 'localhost' || false;
 
     if (this.mock) {
       return;
@@ -56,13 +56,18 @@ export default class UnleashService {
   }
 
   getFeatures(): Features {
-    const features = Object.keys(this.supportedFeatures).reduce(
-      (acc, feature: string) => {
-        if (this.mock) return { ...acc, [feature]: true };
-        const isEnabled = this.unleashInstance?.isSynchronized()
-          ? this.unleashInstance.isEnabled(feature)
-          : this.supportedFeatures[feature];
-        return { ...acc, [feature]: isEnabled };
+    const features = Object.keys(this.defaultToggles).reduce(
+      (acc, toggle: string) => {
+        // If mocking or running from localhost, return default toggles without
+        // actually trying to get anything from Unleash Next
+        if (this.mock) return { ...acc, [toggle]: this.defaultToggles[toggle] };
+
+        // If Unleash instance is not synched, return the default toggles
+        // to prevent chatbot or screen sharing to be disabled just because Unleash failing.
+        const isFeatureEnabled = this.unleashInstance?.isSynchronized()
+          ? this.unleashInstance.isEnabled(toggle)
+          : this.defaultToggles[toggle];
+        return { ...acc, [toggle]: isFeatureEnabled };
       },
       {},
     ) as Features;
