51de7d31b4c444ac7bc1b82ec372ff66d15cbdda
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-11-17 17:08:33 +0100

    Tweaked typescript config for tests

--- packages/shared/test/params.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'bun:test';
-
-describe('Defaut values should not be altered', () => {
-  it('should not alter default values', () => {
-    const params = paramsSchema.safeParse({});
-    console.log(params);
-  });
-});
