7a02a54ddb054934bb38f221bbd222f79c287200
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-11-24 10:12:18 +0100

    Remove credentials include

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -150,10 +150,7 @@ async function populateLoggedInMenu(authObject: Auth) {
       ...window.__DECORATOR_DATA__.params,
       name: authObject.name,
       level: `Level${authObject.securityLevel}` as LoginLevel,
-    })}`,
-   {
-    credentials: 'include',
-  })
+    })}`)
     .then((res) => res.text())
     .then((html) => {
       const userMenu = document.querySelector('user-menu');
