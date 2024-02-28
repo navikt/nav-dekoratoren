3f8673c8f4667db8c89e3df5117d1134b0707453
anders-nom <anders@nomerstad.com>
2024-02-28 12:56:39 +0100

    Gjør litt færre elementer sticky

--- packages/server/src/views/index.ts
+++ packages/server/src/views/index.ts
@@ -87,13 +87,13 @@ export const getEnvAssets = async () => {
 const assets = await getEnvAssets();
 
 export function Index({
-    language,
-    header,
-    footer,
-    decoratorData,
-    maskDocument = false,
-    main,
-}: {
+                          language,
+                          header,
+                          footer,
+                          decoratorData,
+                          maskDocument = false,
+                          main,
+                      }: {
     language: Language;
     header: Template;
     footer: Template;
@@ -106,30 +106,31 @@ export function Index({
     return html`
         <!doctype html>
         <html lang="${language}" ${maskDocument ? 'data-hj-supress' : ''}>
-            <head>
-                <title>${'NAV Dekoratør'}</title>
-                <link
-                    rel="preload"
-                    href="https://cdn.nav.no/aksel/fonts/SourceSans3-normal.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossorigin="anonymous"
-                />
-                <meta charset="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </head>
-            <body>
-                <div id="styles" style="display:none">${unsafeHtml(links)}</div>
-                <div id="header-withmenu">
-                            ${Sticky({ children: header })}</div>
-                <main>
-                    ${Button({
-                        text: 'Test amplitude!',
-                        variant: 'primary',
-                        id: 'amplitude-test',
-                    })}
-                    <button
-                        onclick="(() => {
+        <head>
+            <title>${'NAV Dekoratør'}</title>
+            <link
+                rel="preload"
+                href="https://cdn.nav.no/aksel/fonts/SourceSans3-normal.woff2"
+                as="font"
+                type="font/woff2"
+                crossorigin="anonymous"
+            />
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </head>
+        <body>
+        <div id="styles" style="display:none">${unsafeHtml(links)}</div>
+        <div id="header-withmenu">
+            ${header}
+        </div>
+        <main>
+            ${Button({
+                text: 'Test amplitude!',
+                variant: 'primary',
+                id: 'amplitude-test',
+            })}
+            <button
+                onclick="(() => {
                 window.postMessage({
                   source: 'decoratorClient',
                   event: 'params',
@@ -139,11 +140,11 @@ export function Index({
                   },
                 })
               })()"
-                    >
-                        Set breadcrumbs
-                    </button>
-                    <button
-                        onclick="(() => {
+            >
+                Set breadcrumbs
+            </button>
+            <button
+                onclick="(() => {
               window.postMessage({
                   source: 'decoratorClient',
                   event: 'params',
@@ -153,11 +154,11 @@ export function Index({
                   },
                 })
               })()"
-                    >
-                        Set available languages
-                    </button>
-                    <button
-                        onclick="(() => {
+            >
+                Set available languages
+            </button>
+            <button
+                onclick="(() => {
               window.postMessage({
                   source: 'decoratorClient',
                   event: 'params',
@@ -166,34 +167,34 @@ export function Index({
                   },
                 })
               })()"
-                    >
-                        Markup was updated
-                    </button>
-                    <div>${main}</div>
-                    <script>
-                        window.addEventListener('message', (e) => {
-                            if (e.data.source === 'decorator') {
-                                if (e.data.event === 'languageSelect') {
-                                    window.postMessage({
-                                        source: 'decoratorClient',
-                                        event: 'params',
-                                        payload: {
-                                            language: e.data.payload.locale,
-                                        },
-                                    });
-                                }
-                            }
-                        });
-                    </script>
-                </main>
-                <div id="footer-withmenu">${footer}</div>
-                <div id="scripts" style="display:none">
-                    ${unsafeHtml(scripts)}${decoratorData}
-                    <script>
-                        window.__DECORATOR_DATA__ = JSON.parse(document.getElementById('__DECORATOR_DATA__')?.innerHTML ?? '');
-                    </script>
-                </div>
-            </body>
+            >
+                Markup was updated
+            </button>
+            <div>${main}</div>
+            <script>
+                window.addEventListener('message', (e) => {
+                    if (e.data.source === 'decorator') {
+                        if (e.data.event === 'languageSelect') {
+                            window.postMessage({
+                                source: 'decoratorClient',
+                                event: 'params',
+                                payload: {
+                                    language: e.data.payload.locale,
+                                },
+                            });
+                        }
+                    }
+                });
+            </script>
+        </main>
+        <div id="footer-withmenu">${footer}</div>
+        <div id="scripts" style="display:none">
+            ${unsafeHtml(scripts)}${decoratorData}
+            <script>
+                window.__DECORATOR_DATA__ = JSON.parse(document.getElementById('__DECORATOR_DATA__')?.innerHTML ?? '');
+            </script>
+        </div>
+        </body>
         </html>
     `;
 }
--- packages/server/src/views/sticky.ts
+++ packages/server/src/views/sticky.ts
@@ -2,8 +2,9 @@ import html, { Template } from 'decorator-shared/html';
 import cls from 'decorator-client/src/styles/sticky.module.css';
 
 export function Sticky({ children }: { children: Template }) {
-    return html` <d-sticky class="${cls.stickyContainer}">
-        <div class="${cls.stickyPlaceholder}"></div>
-        <div class="${cls.stickyContent}">${children}</div>
-    </d-sticky>`;
+    return html`
+        <d-sticky class="${cls.stickyContainer}">
+            <div class="${cls.stickyPlaceholder}"></div>
+            <div class="${cls.stickyContent}">${children}</div>
+        </d-sticky>`;
 }
