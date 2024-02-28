e525c4e0867d11ddd2318ae8c3485662b228b68a
anders-nom <anders@nomerstad.com>
2024-02-28 16:04:35 +0100

    Skriver om sticky til en løsning med fixed position

--- packages/client/src/views/sticky.ts
+++ packages/client/src/views/sticky.ts
@@ -8,61 +8,59 @@ class Sticky extends HTMLElement {
     delta: number;
     // Can set this dynamically and reattach listeners
     scrollYCutOff: number;
-    inProgress: boolean;
-    initialized: boolean;
-    isShowing: boolean;
+
+    directionChangedPosition: number;
+    prevScrollDirection: 'up' | 'down' | null;
 
     constructor() {
         super();
         this.prevScrollOffset = 0;
-        this.delta = 40;
-        this.scrollYCutOff = 160;
-        this.inProgress = false;
-        this.initialized = false;
-        this.isShowing = false;
+        this.delta = 20;
+        this.scrollYCutOff = 80;
+
+        this.directionChangedPosition = 0;
+        this.prevScrollDirection = null;
 
         this.stickyContent = this.querySelector(`.${cls.stickyContent}`)!;
-
-        console.log(this.stickyContent);
     }
 
 
     handleScroll = () => {
         console.log('scrolling');
         const currentScrollOffset = window.scrollY;
-        const isScrollingUp = this.prevScrollOffset > currentScrollOffset;
+        const currentScrollDirection = this.prevScrollOffset > currentScrollOffset ? 'up' : 'down';
 
+        this.prevScrollOffset = currentScrollOffset;
 
-        if (window.scrollY === 0) {
-            this.stickyContent.style.position = 'absolute';
-            this.stickyContent.classList.remove(cls.transitioning);
+        if (currentScrollDirection !== this.prevScrollDirection) {
+            this.prevScrollDirection = currentScrollDirection;
+            this.directionChangedPosition = currentScrollOffset;
+            return;
         }
 
-
-        if (currentScrollOffset < this.scrollYCutOff && !this.isShowing) {
-            this.stickyContent.style.position = 'absolute';
-        } else if (isScrollingUp) {
-            this.stickyContent.style.transform = 'translateY(0)';
-            this.isShowing = true;
-        } else if (!isScrollingUp && currentScrollOffset > this.scrollYCutOff) {
-            this.isShowing = false;
-            this.stickyContent.style.transform = 'translateY(-100%)';
-            this.stickyContent.style.position = 'fixed';
+        if (Math.abs(this.directionChangedPosition - currentScrollOffset) < this.delta) {
+            return;
         }
 
-        if (currentScrollOffset > this.scrollYCutOff && this.initialized) {
-            this.stickyContent.classList.add(cls.transitioning);
+        if (currentScrollOffset < this.scrollYCutOff) {
+            this.stickyContent.classList.remove(cls.hidden);
+            return;
         }
 
-        this.initialized = true;
-        this.prevScrollOffset = window.scrollY;
-    }
+        if (currentScrollDirection === 'down') {
+            this.stickyContent.classList.add(cls.hidden);
+        } else {
+            this.stickyContent.classList.remove(cls.hidden);
+        }
+    };
 
     connectedCallback() {
         // this.prevScrollOffset = window.scrollY;
         // this.style.position = 'absolute';
         this.stickyContent = this.querySelector(`.${cls.stickyContent}`)!;
         this.prevScrollOffset = window.scrollY;
+
+        // this.handleScroll()
 
         window.addEventListener('scroll', this.handleScroll);
 
@@ -73,78 +71,4 @@ class Sticky extends HTMLElement {
     }
 }
 
-
 customElements.define('d-sticky', Sticky);
-// import React, { useEffect, useRef } from 'react';
-// import { stickyScrollHandler } from 'komponenter/header/header-regular/common/sticky/StickyUtils';
-// import { focusOverlapHandler } from 'komponenter/header/header-regular/common/sticky/StickyUtils';
-// import { deferStickyOnAnchorLinkHandler } from 'komponenter/header/header-regular/common/sticky/StickyUtils';
-// import style from './Sticky.module.scss';
-//
-// type Props = {
-//     mobileFixed?: boolean;
-//     children: JSX.Element;
-// };
-//
-// export const Sticky = ({ mobileFixed, children }: Props) => {
-//     const prevScrollOffset = useRef(0);
-//     const placeholderRef = useRef<HTMLDivElement>(null);
-//     const stickyRef = useRef<HTMLDivElement>(null);
-//
-//     useEffect(() => {
-//         if (stickyRef.current) {
-//             prevScrollOffset.current = window.pageYOffset;
-//             stickyRef.current.style.position = 'absolute';
-//         }
-//     }, []);
-//
-//     useEffect(() => {
-//         const placeholderElement = placeholderRef.current;
-//         const stickyElement = stickyRef.current;
-//         if (!placeholderElement || !stickyElement) {
-//             return;
-//         }
-//
-//         const setStickyOffset = stickyScrollHandler(prevScrollOffset, stickyElement, placeholderElement);
-//         const deferStickyOnAnchorLinkClick = deferStickyOnAnchorLinkHandler(
-//             prevScrollOffset,
-//             stickyElement,
-//             setStickyOffset
-//         );
-//         const setFocusScrollOffset = focusOverlapHandler(stickyElement);
-//
-//         const activateStickyHeader = () => {
-//             setStickyOffset();
-//             window.addEventListener('focusin', setFocusScrollOffset);
-//             window.addEventListener('scroll', setStickyOffset);
-//             window.addEventListener('resize', setStickyOffset);
-//             window.addEventListener('click', deferStickyOnAnchorLinkClick);
-//         };
-//
-//         if (document.readyState === 'complete') {
-//             activateStickyHeader();
-//         } else {
-//             window.addEventListener('load', activateStickyHeader);
-//         }
-//
-//         return () => {
-//             window.removeEventListener('load', activateStickyHeader);
-//             window.removeEventListener('focusin', setFocusScrollOffset);
-//             window.removeEventListener('scroll', setStickyOffset);
-//             window.removeEventListener('resize', setStickyOffset);
-//             window.removeEventListener('click', deferStickyOnAnchorLinkClick);
-//         };
-//     }, []);
-//
-//     return (
-//         <div className={style.stickyPlaceholder} ref={placeholderRef}>
-//             <div
-//                 className={`${style.stickyContainer} ${mobileFixed ? style.stickyContainerMobilFixed : ''}`}
-//                 ref={stickyRef}
-//             >
-//                 {children}
-//             </div>
-//         </div>
-//     );
-// };
-//
