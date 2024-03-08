b79753f06e19ff73c5980cebdffe68498c6bab85
anders-nom <anders@nomerstad.com>
2024-03-08 17:34:15 +0100

    Fjerner ubrukte auth-funksjoner fra user-menu

--- packages/client/src/views/user-menu.ts
+++ packages/client/src/views/user-menu.ts
@@ -1,15 +1,9 @@
 import { CustomEvents } from '../events';
-import { Auth, AuthLoggedIn } from '../api';
 import { ClientSideCache } from '../helpers/cache';
 import { param } from '../params';
 
 class UserMenu extends HTMLElement {
     private readonly responseCache = new ClientSideCache();
-
-    // TODO: use a global auth state instead?
-    private authState: Auth = {
-        authenticated: false,
-    };
 
     private async fetchMenuHtml() {
         const url = window.makeEndpoint('/user-menu');
@@ -23,12 +17,8 @@ class UserMenu extends HTMLElement {
         return `${param('context')}_${param('language')}`;
     }
 
+    // TODO: some sort of placeholder view while awaiting server response?
     private async populateLoggedInMenu() {
-        // TODO: some sort of placeholder while awaiting auth state?
-        if (!this.authState.authenticated) {
-            return;
-        }
-
         const cacheKey = this.buildCacheKey();
 
         this.responseCache
@@ -37,18 +27,12 @@ class UserMenu extends HTMLElement {
                 if (!html) {
                     // TODO: better error handling
                     console.error('Failed to fetch content for user-menu');
-                    this.innerHTML = 'Kunne ikke laste innlogget meny';
                     return;
                 }
 
                 this.innerHTML = html;
             });
     }
-
-    private updateAuthState = (e: CustomEvent<CustomEvents['authupdated']>) => {
-        this.authState = e.detail.auth;
-        this.populateLoggedInMenu();
-    };
 
     private updateMenu = (e: CustomEvent<CustomEvents['paramsupdated']>) => {
         if (e.detail.params?.context) {
@@ -58,12 +42,10 @@ class UserMenu extends HTMLElement {
 
     private connectedCallback() {
         window.addEventListener('paramsupdated', this.updateMenu);
-        window.addEventListener('authupdated', this.updateAuthState);
     }
 
     private disconnectedCallback() {
         window.removeEventListener('paramsupdated', this.updateMenu);
-        window.removeEventListener('authupdated', this.updateAuthState);
     }
 }
 
