eb0a169379eb47298fa6f471b9b678ae12ee1293
Andreas Nordahl <andnorda@gmail.com>
2024-04-27 10:24:35 +0200

    fix header menu styling issues

--- packages/client/src/views/menu-background.ts
+++ packages/client/src/views/menu-background.ts
@@ -10,7 +10,6 @@ class MenuBackground extends HTMLElement {
             this.openCount = this.openCount + 1;
             if (this.openCount > 0) {
                 this.classList.add(cls.active);
-                document.body.classList.add(cls.menuOpenBody);
             }
         });
 
@@ -18,7 +17,6 @@ class MenuBackground extends HTMLElement {
             this.openCount = this.openCount - 1;
             if (this.openCount < 1) {
                 this.classList.remove(cls.active);
-                document.body.classList.remove(cls.menuOpenBody);
             }
         });
     }
