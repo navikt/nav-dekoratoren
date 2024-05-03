cead7686d6222ed14eead64fd4f856557eed6b13
Anders Nomerstad <anders@nomerstad.com>
2024-05-03 10:42:14 +0200

    Oppdaterer skiplink render ved dom mutations

--- packages/client/src/views/skip-link.ts
+++ packages/client/src/views/skip-link.ts
@@ -3,14 +3,26 @@ import { LenkeMedSporingElement } from "./lenke-med-sporing";
 const MAINCONTENT_ID = "maincontent";
 
 class SkipLinkElement extends LenkeMedSporingElement {
+    // Handles client-side rendering of the maincontent element
+    // TODO: Is this worth the potential performance hit? :|
+    private readonly observer = new MutationObserver(() => this.update());
+
     private update() {
         const hasMainContentElement = !!document.getElementById(MAINCONTENT_ID);
         this.style.display = hasMainContentElement ? "" : "none";
     }
 
-    connectedCallback() {
-        console.log("Connected");
+    private connectedCallback() {
         this.update();
+        this.observer.observe(document.body, {
+            attributeFilter: ["id"],
+            childList: true,
+            subtree: true,
+        });
+    }
+
+    private disconnectedCallback() {
+        this.observer.disconnect();
     }
 }
 
