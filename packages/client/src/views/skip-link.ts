9c3f93d62795300e3d84e2832eed0557636ed031
Anders Nomerstad <anders@nomerstad.com>
2024-05-03 10:43:23 +0200

    Metodenavn

--- packages/client/src/views/skip-link.ts
+++ packages/client/src/views/skip-link.ts
@@ -5,15 +5,17 @@ const MAINCONTENT_ID = "maincontent";
 class SkipLinkElement extends LenkeMedSporingElement {
     // Handles client-side rendering of the maincontent element
     // TODO: Is this worth the potential performance hit? :|
-    private readonly observer = new MutationObserver(() => this.update());
+    private readonly observer = new MutationObserver(() =>
+        this.updateVisibility(),
+    );
 
-    private update() {
+    private updateVisibility() {
         const hasMainContentElement = !!document.getElementById(MAINCONTENT_ID);
         this.style.display = hasMainContentElement ? "" : "none";
     }
 
     private connectedCallback() {
-        this.update();
+        this.updateVisibility();
         this.observer.observe(document.body, {
             attributeFilter: ["id"],
             childList: true,
