b26c3b3e5dbca8a304bfca4a4321483a66e684c1
Andreas Nordahl <andnorda@gmail.com>
2024-05-10 13:02:59 +0200

    Rewrite read more (#271)
    Fixes https://github.com/navikt/decorator-next/issues/146

--- packages/shared/views/read-more.ts
+++ packages/shared/views/read-more.ts
@@ -1,19 +1,20 @@
-import html from "../html";
-
 import cls from "decorator-client/src/styles/read-more.module.css";
+import html, { Template } from "../html";
+import { DownChevronIcon } from "./icons";
 
-type ReadMoreProps = {
+export type ReadMoreProps = {
     header: string;
-    content: string[];
+    content: Template;
 };
 
 export const ReadMore = (props: ReadMoreProps) => {
     return html`
         <details class="${cls.details}">
-            <summary class="${cls.summary}">${props.header}</summary>
-            <div class="${cls.answer}">
-                ${props.content.map((a) => html`<p>${a}</p>`)}
-            </div>
+            <summary class="${cls.summary}">
+                ${DownChevronIcon({ className: cls.icon })}
+                <div>${props.header}</div>
+            </summary>
+            <div class="${cls.answer}">${props.content}</div>
         </details>
     `;
 };
