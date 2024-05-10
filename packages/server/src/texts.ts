70083d7259ad7643bbc15bc526f2f77772f9edfa
Anders Nomerstad <anders@nomerstad.com>
2024-05-10 13:51:36 +0200

    Rydder i texts. Bruker engelske tekster for pl/uk/ru

--- packages/server/src/texts.ts
+++ packages/server/src/texts.ts
@@ -1,16 +1,6 @@
 import { Language } from "decorator-shared/params";
 import { Texts } from "decorator-shared/types";
 
-// 'delskjerm-modal-hjelpetekst-0': `Når du deler skjerm med NAV kontaktsenter kan veilederen hjelpe deg med å finne fram på nav.no.`,
-// 'delskjerm-modal-hjelpetekst-0-en': `When you share your screen, the counsellor can help you navigate nav.no`,
-// 'delskjerm-modal-hjelpetekst-0-se': `Når du deler skjerm med NAV kontaktsenter kan veilederen hjelpe deg med å finne fram på nav.no.`,
-// 'delskjerm-modal-hjelpetekst-1': `Veilederen ser kun det du ser på nav.no og kan ikke fylle inn opplysninger eller sende inn noe på dine vegne. `,
-// 'delskjerm-modal-hjelpetekst-1-en': `The counsellor can only see what you see on nav.no and can't fill in any information or send anything on your behalf.`,
-// 'delskjerm-modal-hjelpetekst-1-se': `Veilederen ser kun det du ser på nav.no og kan ikke fylle inn opplysninger eller sende inn noe på dine vegne. `,
-// 'delskjerm-modal-hjelpetekst-2': `Det er du som godkjenner skjermdeling. Ingenting blir lagret.`,
-// 'delskjerm-modal-hjelpetekst-2-en': `Screen sharing must be approved by you. Nothing is stored.`,
-// 'delskjerm-modal-hjelpetekst-2-se': `Det er du som godkjenner skjermdeling. Ingenting blir lagret.`,
-
 const nb = {
     skip_link: "Hopp til hovedinnhold",
     share_screen: "Del skjerm med veileder",
@@ -92,98 +82,96 @@ const nb = {
         "Du har logget inn med Min ID. Hvis du logger inn med et høyere sikkerhetsnivå, får du se mer innhold og flere tjenester.",
     go_to_my_page: "Gå til Min side",
     loading: "Laster",
-} satisfies Texts;
+} as const satisfies Texts;
 
-export type LangBaseKeys = keyof typeof nb;
+const en = {
+    skip_link: "Go to main content",
+    share_screen: "Share screen with your counsellor",
+    to_top: "To the top",
+    menu: "Menu",
+    close: "Close",
+    did_you_find: "Fant du det du lette etter?",
+    search: "Search",
+    search_nav_no: "Search nav.no",
+    clear: "Clear",
+    login: "Log in",
+    logout: "Log out",
+    logged_in: "Logged in",
+    language_selector: "Choose language",
+    notifications: "Notifications",
+    notifications_empty_list: "You have no new notifications",
+    notifications_empty_list_description:
+        "We will notify you when something happens.",
+    notifications_show_all: "Previous notifications",
+    notifications_messages_title: "Beskjeder",
+    notified_EPOST: "Notified by e-mail",
+    notified_SMS: "Notified by SMS",
+    notified_SMS_and_EPOST: "Notified by e-mail and SMS",
+    earlier_notifications: "Earlier notifications",
+    message: "Message",
+    task: "Task",
+    masked_message_text:
+        "You have a message, please log in with a higher security level to read the message.",
+    masked_task_text:
+        "You have a task, please log in with a higher security level to see the task.",
+    archive: "Archive",
+    notifications_tasks_title: "Tasks",
+    token_warning_title: "You will soon be logged out automatically",
+    token_warning_body: "Would you like to stay logged in?",
+    session_warning_title:
+        "You will be logged out automatically in about $1 minutes",
+    session_warning_body: "Avslutt det du jobber med og logg inn igjen.",
+    yes: "Yes",
+    no: "No",
+    ok: "OK",
+    hensikt_med_tilbakemelding:
+        "Unfortunately you will not get a reply to your feedback. Do you have questions or need help?",
+    hensikt_med_tilbakemelding_lenke: "Call, chat or write to us",
+    send_undersokelse_takk: "Thanks!",
+    rolle_privatperson: "Private",
+    rolle_arbeidsgiver: "Employer",
+    rolle_samarbeidspartner: "Collaborator",
+    meny_bunnlenke_minside_stikkord:
+        "Your cases, payments, messages, report cards, activity plan, personal information and more services",
+    meny_bunnlenke_arbeidsgiver_stikkord:
+        "Your sick leave, recruitment, digital forms",
+    meny_bunnlenke_samarbeidspartner_stikkord:
+        "Health personnel, intervention organizers, counties and municipalities",
+    loading_notifications: "Loading notifications",
+    notifications_error: "Error loading notifications",
+    how_can_we_help: "How can we help you?",
+    showing: "Showing",
+    of: "of",
+    results: "results",
+    see_all_hits: "See all hits",
+    no_hits_for: "No hits for",
+    loading_preview: "Loading preview",
+    to_front_page: "To the front page",
+    important_info: "Imporant information: ",
+    footer_del_skjerm: "Share screen with your counsellor",
+    delskjerm_modal_beskrivelse: `Give the counsellor access to see the same content as you on nav.no`,
+    delskjerm_modal_start: "Start screen sharing",
+    delskjerm_modal_label: "Enter the code you received from the counsellor",
+    delskjerm_modal_avbryt: "Cancel",
+    delskjerm_modal_feilmelding: "Must be 5 digits",
+    delskjerm_modal_hjelpetekst_overskrift: "What is screen sharing?",
+    delskjerm_modal_hjelpetekst_0:
+        "When you share your screen, the counsellor can help you navigate nav.no",
+    delskjerm_modal_hjelpetekst_1:
+        "The counsellor can only see what you see on nav.no and can't fill in any information or send anything on your behalf.",
+    delskjerm_modal_hjelpetekst_2:
+        "Screen sharing must be approved by you. Nothing is stored.",
+    delskjerm_modal_stengt:
+        "Screen sharing is currently closed, please try again later",
+    security_level_info:
+        "You are logged in with Min ID. If you log in with a higher security level, you will see more content and additional services.",
+    go_to_my_page: "Go to my page",
+    loading: "Loading",
+} as const satisfies Texts;
 
 export const texts: Record<Language, Texts> = {
     nb,
-    en: {
-        skip_link: "Go to main content",
-        share_screen: "Share screen with your counsellor",
-        to_top: "To the top",
-        menu: "Menu",
-        close: "Close",
-        did_you_find: "Fant du det du lette etter?",
-        search: "Search",
-        search_nav_no: "Search nav.no",
-        clear: "Clear",
-        login: "Log in",
-        logout: "Log out",
-        logged_in: "Logged in",
-        language_selector: "Choose language",
-        notifications: "Notifications",
-        notifications_empty_list: "You have no new notifications",
-        notifications_empty_list_description:
-            "We will notify you when something happens.",
-        notifications_show_all: "Previous notifications",
-        notifications_messages_title: "Beskjeder",
-        notified_EPOST: "Notified by e-mail",
-        notified_SMS: "Notified by SMS",
-        notified_SMS_and_EPOST: "Notified by e-mail and SMS",
-        earlier_notifications: "Earlier notifications",
-        message: "Message",
-        task: "Task",
-        masked_message_text:
-            "You have a message, please log in with a higher security level to read the message.",
-        masked_task_text:
-            "You have a task, please log in with a higher security level to see the task.",
-        archive: "Archive",
-        notifications_tasks_title: "Tasks",
-        token_warning_title: "You will soon be logged out automatically",
-        token_warning_body: "Would you like to stay logged in?",
-        session_warning_title:
-            "You will be logged out automatically in about $1 minutes",
-        session_warning_body: "Avslutt det du jobber med og logg inn igjen.",
-        yes: "Yes",
-        no: "No",
-        ok: "OK",
-        hensikt_med_tilbakemelding:
-            "Unfortunately you will not get a reply to your feedback. Do you have questions or need help?",
-        hensikt_med_tilbakemelding_lenke: "Call, chat or write to us",
-        send_undersokelse_takk: "Thanks!",
-        rolle_privatperson: "Private",
-        rolle_arbeidsgiver: "Employer",
-        rolle_samarbeidspartner: "Collaborator",
-        meny_bunnlenke_minside_stikkord:
-            "Your cases, payments, messages, report cards, activity plan, personal information and more services",
-        meny_bunnlenke_arbeidsgiver_stikkord:
-            "Your sick leave, recruitment, digital forms",
-        meny_bunnlenke_samarbeidspartner_stikkord:
-            "Health personnel, intervention organizers, counties and municipalities",
-        loading_notifications: "Loading notifications",
-        notifications_error: "Error loading notifications",
-        how_can_we_help: "How can we help you?",
-        showing: "Showing",
-        of: "of",
-        results: "results",
-        see_all_hits: "See all hits",
-        no_hits_for: "No hits for",
-        loading_preview: "Loading preview",
-        to_front_page: "To the front page",
-        important_info: "Imporant information: ",
-        footer_del_skjerm: "Share screen with your counsellor",
-        delskjerm_modal_beskrivelse: `Give the counsellor access to see the same content as you on nav.no`,
-        delskjerm_modal_start: "Start screen sharing",
-        delskjerm_modal_label:
-            "Enter the code you received from the counsellor",
-        delskjerm_modal_avbryt: "Cancel",
-        delskjerm_modal_feilmelding: "Must be 5 digits",
-        delskjerm_modal_hjelpetekst_overskrift: "What is screen sharing?",
-        delskjerm_modal_hjelpetekst_0:
-            "When you share your screen, the counsellor can help you navigate nav.no",
-        delskjerm_modal_hjelpetekst_1:
-            "The counsellor can only see what you see on nav.no and can't fill in any information or send anything on your behalf.",
-        delskjerm_modal_hjelpetekst_2:
-            "Screen sharing must be approved by you. Nothing is stored.",
-        delskjerm_modal_stengt:
-            "Screen sharing is currently closed, please try again later",
-        // TODO: dobbelsjekk oversettelse
-        security_level_info:
-            "You are logged in with Min ID. If you log in with a higher security level, you will see more content and additional services.",
-        go_to_my_page: "Go to my page",
-        loading: "Loading",
-    },
+    en,
     se: {
         ...nb,
         search: "Oza",
@@ -191,7 +179,7 @@ export const texts: Record<Language, Texts> = {
         logout: "Olggosčáliheapmi",
     },
     nn: nb,
-    pl: nb,
-    uk: nb,
-    ru: nb,
+    pl: en,
+    uk: en,
+    ru: en,
 };
