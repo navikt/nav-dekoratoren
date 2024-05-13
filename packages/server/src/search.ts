309b3ca3446526ccc3d7332e0c519ed2d7b3ed50
Anders Nomerstad <anders@nomerstad.com>
2024-05-13 20:08:11 +0200

    Fjerner unødvendig mapping

--- packages/server/src/search.ts
+++ packages/server/src/search.ts
@@ -14,10 +14,6 @@ export const search = ({
         `${env.SEARCH_API}?ord=${encodeURIComponent(query)}&f=${context}&preferredLanguage=${language}`,
     )
         .then((res) => res.json() as Promise<SearchResult>)
-        .then((result) => ({
-            hits: result.hits.slice(0, 5),
-            total: result.total,
-        }))
         .catch((err) => {
             console.error(`Error from search api - ${err}`);
 
