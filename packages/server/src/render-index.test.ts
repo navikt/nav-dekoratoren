c2a95a3197c9e5da9f1f8f2f0608eb0f28366ca2
Anders Nomerstad <anders@nomerstad.com>
2024-05-14 13:15:11 +0200

    Fix mock server

--- packages/server/src/render-index.test.ts
+++ packages/server/src/render-index.test.ts
@@ -1,34 +1,21 @@
-import {
-    afterAll,
-    afterEach,
-    beforeAll,
-    beforeEach,
-    expect,
-    test,
-} from "bun:test";
+import { afterAll, expect, test } from "bun:test";
 import renderIndex from "./render-index";
 import UnleashService from "./unleash-service";
 import { http, HttpResponse } from "msw";
 import { env } from "./env/server";
 import testData from "./content-test-data.json";
-import { clearCache } from "decorator-shared/cache";
-import { SetupServerApi, setupServer } from "msw/node";
+import { setupServer } from "msw/node";
 
 const unleashService = new UnleashService({ mock: true });
 
 test("It masks the document from hotjar", async () => {
-    let server: SetupServerApi;
+    const server = setupServer(
+        http.get(`${env.ENONICXP_SERVICES}/no.nav.navno/menu`, () =>
+            HttpResponse.json(testData),
+        ),
+    );
+    server.listen();
 
-    beforeAll(() => {
-        server = setupServer(
-            http.get(`${env.ENONICXP_SERVICES}/no.nav.navno/menu`, () =>
-                HttpResponse.json(testData),
-            ),
-        );
-        server.listen();
-    });
-    beforeEach(() => clearCache());
-    afterEach(() => server.resetHandlers());
     afterAll(() => server.close());
 
     expect(
