fed3d338880ec71042752e3dafdc9d2bd66cac5f
Anders Nomerstad <anders@nomerstad.com>
2024-05-14 14:54:05 +0200

    Generisk funksjon for fetch + validering

---
+++ packages/server/src/lib/fetch-and-validate.ts
@@ -0,0 +1,42 @@
+import { Result, ResultType } from "../result";
+import { ZodSchema } from "zod";
+
+type FetchAndValidate = <ResponseData>(
+    ...args: [...Parameters<typeof fetch>, schema: ZodSchema]
+) => Promise<ResultType<ResponseData>>;
+
+const parseAndValidateResponse = <ResponseData>(
+    response: unknown,
+    schema: ZodSchema<ResponseData>,
+): ResultType<ResponseData> => {
+    const validatedResponse = schema.safeParse(response);
+
+    if (!validatedResponse.success) {
+        return Result.Error(
+            `Error parsing response - ${validatedResponse.error}`,
+        );
+    }
+
+    return Result.Ok(validatedResponse.data);
+};
+
+export const fetchAndValidateJson: FetchAndValidate = async (
+    url,
+    init,
+    schema,
+) =>
+    fetch(url, init)
+        .then((res) => {
+            if (!res.ok) {
+                return Result.Error(
+                    `Bad response from ${url}: ${res.status} - ${res.statusText}`,
+                );
+            }
+
+            return res
+                .json()
+                .then((json) => parseAndValidateResponse(json, schema));
+        })
+        .catch((err) => {
+            return Result.Error(`Failed to fetch from ${url}: ${err}`);
+        });
