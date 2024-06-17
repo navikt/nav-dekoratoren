50b6f358f9f1bd70f6296790589a20015db5c89d
Skifjeld <eivind.skifjeld@nav.no>
2024-06-17 09:59:39 +0200

    Fjerne tom list i search-hits

--- packages/server/src/views/search-hits.ts
+++ packages/server/src/views/search-hits.ts
@@ -30,41 +30,47 @@ export const SearchHits = ({
                 ${texts.change_search_filter}
             </a>
         </div>
-        <ul class="${cls.searchHitList}">
-            ${hits.map(
-                (hit, index) => html`
-                    <li>
-                        <lenke-med-sporing
-                            href="${hit.href}"
-                            class="${cls.searchHit}"
-                            data-analytics-event-args="${JSON.stringify({
-                                eventName: "resultat-klikk",
-                                destinasjon: "[redacted]",
-                                sokeord: "[redacted]",
-                                treffnr: index + 1,
-                            })}"
-                        >
-                            ${ForwardChevron({
-                                className: cls.chevron,
-                            })}
-                            <div>
-                                <h2 class="${cls.title}">${hit.displayName}</h2>
-                                <div>${unsafeHtml(hit.highlight)}</div>
-                            </div>
-                        </lenke-med-sporing>
-                    </li>
-                `,
-            )}
-        </ul>
         ${total > 0
-            ? html`<div role="status" class="${cls.searchMoreHits}">
-                  <a href="https://www.nav.no/sok?ord=${query}">
-                      ${texts.more_hits}
-                      ${ArrowRight({
-                          className: cls.searchHitRightArrow,
-                      })}
-                  </a>
-              </div>`
+            ? html` <ul class="${cls.searchHitList}">
+                      ${hits.map(
+                          (hit, index) => html`
+                              <li>
+                                  <lenke-med-sporing
+                                      href="${hit.href}"
+                                      class="${cls.searchHit}"
+                                      data-analytics-event-args="${JSON.stringify(
+                                          {
+                                              eventName: "resultat-klikk",
+                                              destinasjon: "[redacted]",
+                                              sokeord: "[redacted]",
+                                              treffnr: index + 1,
+                                          },
+                                      )}"
+                                  >
+                                      ${ForwardChevron({
+                                          className: cls.chevron,
+                                      })}
+                                      <div>
+                                          <h2 class="${cls.title}">
+                                              ${hit.displayName}
+                                          </h2>
+                                          <div>
+                                              ${unsafeHtml(hit.highlight)}
+                                          </div>
+                                      </div>
+                                  </lenke-med-sporing>
+                              </li>
+                          `,
+                      )}
+                  </ul>
+                  <div role="status" class="${cls.searchMoreHits}">
+                      <a href="https://www.nav.no/sok?ord=${query}">
+                          ${texts.more_hits}
+                          ${ArrowRight({
+                              className: cls.searchHitRightArrow,
+                          })}
+                      </a>
+                  </div>`
             : null}
     </div>
 `;
