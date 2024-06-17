44866713fbf7cbbf4a9a8fdc1ad1e0d4180acef8
Anders Nomerstad <anders@nomerstad.com>
2024-06-17 15:48:05 +0200

    Revert splash-page

--- packages/server/src/views/splash-page.ts
+++ packages/server/src/views/splash-page.ts
@@ -1,20 +1,17 @@
 import html from "decorator-shared/html";
+import { match } from "ts-pattern";
 
 import cls from "decorator-client/src/styles/splash-page.module.css";
-import { match } from "ts-pattern";
 
 function SplashPage() {
     return html`
         <div class="${cls.splashPage}">
-            <div id="blahblah" style="margin-bottom: 8rem">Blah blah</div>
             <h1>Decorator next</h1>
             <div class="${cls.splashAlert}">
                 <span>
                     Hei! Dette er en intern test-side for header og footer på
                     nav.no. <a href="https://www.nav.no">Gå til forsiden</a>.
                 </span>
-                <a href="#decorator-header">Til toppen</a>
-                <a href="#blahblah">Til blah blah</a>
             </div>
         </div>
     `;
