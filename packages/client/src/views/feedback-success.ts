99882de16a80534e95cb60a44c10ec70d6c056ff
Andreas Nordahl <andnorda@gmail.com>
2024-06-24 11:45:20 +0200

    use global link styles in feedback

--- packages/client/src/views/feedback-success.ts
+++ packages/client/src/views/feedback-success.ts
@@ -1,3 +1,4 @@
+import globalCls from "decorator-client/src/styles/global.module.css";
 import html from "decorator-shared/html";
 import type { ClientTexts } from "decorator-shared/types";
 import cls from "../styles/feedback.module.css";
@@ -13,7 +14,7 @@ export function FeedbackSuccess({ texts }: FeedbackSuccessProps) {
                 ${texts.send_undersokelse_takk}
             </h2>
             <div>${texts.hensikt_med_tilbakemelding}</div>
-            <a class="${cls.link}" href="/kontaktoss">
+            <a class="${globalCls.link}" href="/kontaktoss">
                 ${texts.hensikt_med_tilbakemelding_lenke}
             </a>
         </div>
