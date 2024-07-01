94dea057a285773d3554061cb077baac0e303c70
Andreas Nordahl <andnorda@gmail.com>
2024-07-01 13:08:17 +0200

    fix chatbot spec selector

--- tests/chatbot.spec.ts
+++ tests/chatbot.spec.ts
@@ -2,15 +2,16 @@ import { expect } from "@playwright/test";
 import { test } from "./fixtures";
 
 test("chatbot", async ({ page }) => {
-    await expect(page.getByTestId("chatbot-frida-knapp")).not.toBeVisible();
+    await expect(page.getByLabel("Åpne chat")).not.toBeVisible();
     await page.evaluate(() => {
         window.postMessage({
             source: "decoratorClient",
             event: "params",
             payload: {
+                chatbot: true,
                 chatbotVisible: true,
             },
         });
     });
-    await expect(page.getByTestId("chatbot-frida-knapp")).toBeVisible();
+    await expect(page.getByLabel("Åpne chat")).toBeVisible();
 });
