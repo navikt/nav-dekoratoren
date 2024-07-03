80b55747e7112b7eed4ea60520c1d9d561da1d6c
Anders Nomerstad <anders@nomerstad.com>
2024-07-03 15:24:39 +0200

    Kjører customElements.define umiddelbart dersom dom er ready allerede

--- packages/client/src/custom-elements.ts
+++ packages/client/src/custom-elements.ts
@@ -4,31 +4,37 @@ type CustomElementProps = {
     options?: ElementDefinitionOptions;
 };
 
-const customElements: Record<string, CustomElementProps> = {};
+const customElementsInitQueue: Record<string, CustomElementProps> = {};
 
+const initCustomElement = ({ name, element, options }: CustomElementProps) => {
+    try {
+        window.customElements.define(name, element, options);
+    } catch (e) {
+        console.error(`Failed to register custom element for "${name}" - ${e}`);
+    }
+};
+
+// Custom elements should not be defined until the DOM is fully loaded. This prevents
+// certain inconsistent behaviours and potential bugs in the lifecycle of custom elements
 export const registerCustomElement = (
     name: string,
     element: CustomElementConstructor,
     options?: ElementDefinitionOptions,
 ) => {
-    if (customElements[name]) {
+    if (customElementsInitQueue[name]) {
         console.error(`Custom element for ${name} was already registered!`);
         return;
     }
 
-    customElements[name] = { name, element, options };
+    const props = { name, element, options };
+
+    if (document.readyState === "loading") {
+        customElementsInitQueue[name] = props;
+    } else {
+        initCustomElement(props);
+    }
 };
 
-// Custom elements should not be defined until the DOM is fully loaded. This prevents
-// certain inconsistent behaviours and potential bugs in the lifecycle of custom elements
 export const initCustomElements = () => {
-    Object.values(customElements).forEach(({ name, element, options }) => {
-        try {
-            window.customElements.define(name, element, options);
-        } catch (e) {
-            console.error(
-                `Failed to register custom element for "${name}" - ${e}`,
-            );
-        }
-    });
+    Object.values(customElementsInitQueue).forEach(initCustomElement);
 };
--- packages/server/src/views/index.ts
+++ packages/server/src/views/index.ts
@@ -169,7 +169,7 @@ export function Index({
                     </script>
                     ${unsafeHtml(scriptsAsString)}
                 </div>
-
+                <!-- the elements belov are needed for backwards compatibility with certain older implementations -->
                 <div id="skiplinks"></div>
                 <div id="megamenu-resources"></div>
                 <div id="webstats-ga-notrack"></div>
