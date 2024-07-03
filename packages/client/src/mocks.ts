bd2d03109697e8e3e704703ea0f792eba5293733
Terje Karlsen <terjeofnorway@gmail.com>
2024-07-03 12:07:13 +0200

    Forbedre utloggingsvarsel (#350)
    Beregner tid for utloggingsvarsel basert på `expires_in_seconds` og
    regner om dette til lokal tid. Dermed slipper vi problemer hvor brukere
    har feil klokkeslett på maskinen sin.
    
    Rydder i logout-warning for bedre lesbarhet.
    
    Skriver enhetstester for auth-hjelperen.
    
    Flytter også logout-warning ut av controller-mappe (som den eneste
    komponenten der) og legger den i src sammen med de andre.
    
    Foreslår å bumpe vitest fordi gammel versjon ikke fungerer med VS-code.
    
    ---------
    
    Co-authored-by: Andreas Nordahl <andnorda@gmail.com>

--- packages/client/src/mocks.ts
+++ packages/client/src/mocks.ts
@@ -1,33 +1,27 @@
 import { http, HttpResponse } from "msw";
 import { setupWorker } from "msw/browser";
-
-const now = () => new Date().toISOString();
-const oneHourFromNow = () => {
-    const date = new Date();
-    date.setHours(date.getHours() + 1);
-    return date.toISOString();
-};
-const sixHoursFromNow = () => {
-    const date = new Date();
-    date.setHours(date.getHours() + 6);
-    return date.toISOString();
-};
+import {
+    nowISOString,
+    addOneHourFromNow,
+    getSecondsRemaining,
+    addSixHoursFromNow,
+} from "./helpers/time";
 
 export const worker = setupWorker(
     http.get("http://localhost:8089/api/oauth2/session", () =>
         HttpResponse.json({
             session: {
-                created_at: now(),
-                ends_at: sixHoursFromNow(),
-                timeout_at: oneHourFromNow(),
-                ends_in_seconds: 21600,
+                created_at: nowISOString(),
+                ends_at: addSixHoursFromNow(),
+                timeout_at: addOneHourFromNow(),
+                ends_in_seconds: getSecondsRemaining(addSixHoursFromNow()),
                 active: true,
-                timeout_in_seconds: 3600,
+                timeout_in_seconds: getSecondsRemaining(addOneHourFromNow()),
             },
             tokens: {
-                expire_at: oneHourFromNow(),
-                refreshed_at: now(),
-                expire_in_seconds: 3600,
+                expire_at: addOneHourFromNow(),
+                refreshed_at: nowISOString(),
+                expire_in_seconds: getSecondsRemaining(addOneHourFromNow()),
                 next_auto_refresh_in_seconds: -1,
                 refresh_cooldown: true,
                 refresh_cooldown_seconds: 31,
@@ -37,17 +31,17 @@ export const worker = setupWorker(
     http.get("http://localhost:8089/api/oauth2/session/refresh", () =>
         HttpResponse.json({
             session: {
-                created_at: now(),
-                ends_at: sixHoursFromNow(),
-                timeout_at: oneHourFromNow(),
-                ends_in_seconds: 21600,
+                created_at: nowISOString(),
+                ends_at: addSixHoursFromNow(),
+                timeout_at: addOneHourFromNow(),
+                ends_in_seconds: getSecondsRemaining(addSixHoursFromNow()),
                 active: true,
-                timeout_in_seconds: 3600,
+                timeout_in_seconds: getSecondsRemaining(addOneHourFromNow()),
             },
             tokens: {
-                expire_at: oneHourFromNow(),
-                refreshed_at: now(),
-                expire_in_seconds: 3600,
+                expire_at: addOneHourFromNow(),
+                refreshed_at: nowISOString(),
+                expire_in_seconds: getSecondsRemaining(addOneHourFromNow()),
                 next_auto_refresh_in_seconds: -1,
                 refresh_cooldown: true,
                 refresh_cooldown_seconds: 31,
