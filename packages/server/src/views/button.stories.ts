825d31de2b585f5b55fbc32077e3b5057a0ae029
Andreas Nordahl <andnorda@gmail.com>
2024-07-04 12:36:56 +0200

    refactor button stories

--- packages/server/src/views/button.stories.ts
+++ packages/server/src/views/button.stories.ts
@@ -4,50 +4,29 @@ import { LoginIcon, PersonCircleIcon } from "decorator-shared/views/icons";
 import type { ButtonProps } from "./button";
 import { Button } from "./button";
 
+const Buttons = (props: Partial<ButtonProps> = {}) =>
+    html`<div style="display: flex; gap: 1rem;padding: 1rem;">
+        ${["primary", "secondary", "tertiary"].map((variant) =>
+            Button({
+                ...props,
+                content: {
+                    render: () =>
+                        variant.charAt(0).toUpperCase() + variant.slice(1),
+                },
+                variant: variant as ButtonProps["variant"],
+            }),
+        )}
+    </div>`;
+
 const meta: Meta = {
     title: "button",
     tags: ["autodocs"],
     render: () => {
-        return html`<div style="display: flex; gap: 1rem;padding: 1rem;">
-                ${["primary", "secondary", "tertiary"].map((variant) =>
-                    Button({
-                        content: {
-                            render: () =>
-                                variant.charAt(0).toUpperCase() +
-                                variant.slice(1),
-                        },
-                        variant: variant as ButtonProps["variant"],
-                    }),
-                )}
-            </div>
-            <div style="display: flex; gap: 1rem;padding: 1rem;">
-                ${["primary", "secondary", "tertiary"].map((variant) =>
-                    Button({
-                        content: {
-                            render: () =>
-                                variant.charAt(0).toUpperCase() +
-                                variant.slice(1),
-                        },
-                        icon: PersonCircleIcon({}),
-                        variant: variant as ButtonProps["variant"],
-                    }),
-                )}
-            </div>
-
-            <div style="display: flex; gap: 1rem;padding: 1rem;">
-                ${["primary", "secondary", "tertiary"].map((variant) =>
-                    Button({
-                        content: {
-                            render: () =>
-                                variant.charAt(0).toUpperCase() +
-                                variant.slice(1),
-                        },
-                        icon: LoginIcon({}),
-                        variant: variant as ButtonProps["variant"],
-                        href: "#",
-                    }),
-                )}
-            </div> `;
+        return html`${[
+            Buttons(),
+            Buttons({ icon: PersonCircleIcon({}) }),
+            Buttons({ icon: LoginIcon({}), href: "#" }),
+        ]}`;
     },
 };
 
