4811457af79cb00cedc9e03a44b43f8ae467b522
Andreas Nordahl <andnorda@gmail.com>
2024-07-04 12:25:05 +0200

    move hidden to global

--- packages/client/src/views/user-menu.ts
+++ packages/client/src/views/user-menu.ts
@@ -1,15 +1,18 @@
-import cls from "decorator-client/src/styles/user-menu.module.css";
+import cls from "../styles/user-menu.module.css";
+import globalCls from "../styles/global.module.css";
 import { defineCustomElement } from "../custom-elements";
 import { CustomEvents } from "../events";
 
 class UserMenu extends HTMLElement {
     private onAuthUpdated = (e: CustomEvent<CustomEvents["authupdated"]>) => {
         this.classList.add(cls.userMenuContainer);
-        this.querySelector(`.${cls.loader}`)?.classList.add(cls.hidden);
+        this.querySelector(`.${cls.loader}`)?.classList.add(globalCls.hidden);
         if (e.detail.auth.authenticated) {
             this.innerHTML = e.detail.usermenuHtml!;
         } else {
-            this.querySelector("login-button")?.classList.remove(cls.hidden);
+            this.querySelector("login-button")?.classList.remove(
+                globalCls.hidden,
+            );
         }
     };
 
--- packages/server/src/views/user-menu.ts
+++ packages/server/src/views/user-menu.ts
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import akselCls from "decorator-client/src/styles/aksel.module.css";
+import globalCls from "decorator-client/src/styles/global.module.css";
 import cls from "decorator-client/src/styles/user-menu.module.css";
 import html from "decorator-shared/html";
 import { LoginIcon } from "decorator-shared/views/icons";
@@ -11,7 +12,7 @@ export const UserMenu = ({ loginUrl }: { loginUrl: string }) => html`
         <span class="${clsx(cls.loader, akselCls["navds-label"])}">
             ${i18n("loading")}
         </span>
-        <login-button class="${cls.hidden}">
+        <login-button class="${globalCls.hidden}">
             ${HeaderButton({
                 content: i18n("login"),
                 icon: LoginIcon({}),
