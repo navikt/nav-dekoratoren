2a15a745da15c4b6bca67e352ce40a2ab2c18012
Andreas Nordahl <andnorda@gmail.com>
2024-07-04 12:00:41 +0200

    move header button style

--- packages/server/src/views/header-button.ts
+++ packages/server/src/views/header-button.ts
@@ -1,9 +1,9 @@
-import menuItemsCls from "decorator-client/src/styles/menu-items.module.css";
-import { Button, ButtonProps } from "./button";
 import clsx from "clsx";
+import cls from "decorator-client/src/styles/header-button.module.css";
+import { Button, ButtonProps } from "./button";
 
 export const HeaderButton = (props: ButtonProps) =>
     Button({
         ...props,
-        className: clsx(props.className, menuItemsCls.menuItem),
+        className: clsx(props.className, cls.headerButton),
     });
