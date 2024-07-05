254818eb36738c7c50d18587d145b98676e1e23a
Anders Nomerstad <anders@nomerstad.com>
2024-07-05 11:37:07 +0200

    Setter build-id parameter på request-url'er

--- packages/client/src/helpers/urls.ts
+++ packages/client/src/helpers/urls.ts
@@ -1,6 +1,7 @@
 import { Params } from "decorator-shared/params";
 import { formatParams } from "decorator-shared/json";
 import { env } from "../params";
+import { BUILD_ID_HEADER } from "decorator-shared/constants";
 
 export const endpointUrlWithParams = (
     endpointUrl: `/${string}`,
@@ -11,7 +12,7 @@ export const endpointUrlWithParams = (
         ...params,
     });
 
-    return `${env("APP_URL")}${endpointUrl}?${formattedParams}`;
+    return `${env("APP_URL")}${endpointUrl}?${formattedParams}&${BUILD_ID_HEADER}=${env("BUILD_ID")}`;
 };
 
 export const cdnUrl = (url: string) => `${env("CDN_URL")}${url}`;
