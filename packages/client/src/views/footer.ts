ffa2c0b07a1c568cb4c2f91f3eaf805d6b8695f0
Andreas Nordahl <andnorda@gmail.com>
2024-07-10 16:26:51 +0200

    footer

--- packages/client/src/views/footer.ts
+++ packages/client/src/views/footer.ts
@@ -1,5 +1,7 @@
+import { amplitudeEvent } from "../analytics/amplitude";
 import { defineCustomElement } from "../custom-elements";
 import { endpointUrlWithParams } from "../helpers/urls";
+import { param } from "../params";
 
 class Footer extends HTMLElement {
     handleParamsUpdated = (e: CustomEvent) => {
@@ -11,6 +13,21 @@ class Footer extends HTMLElement {
     };
 
     connectedCallback() {
+        this.addEventListener("click", (event: MouseEvent) => {
+            if (event.target instanceof HTMLAnchorElement) {
+                const url = event.target.getAttribute("href");
+
+                if (url && url !== "#") {
+                    amplitudeEvent({
+                        context: param("context"),
+                        action: `kontakt/${url}`,
+                        category: "dekorator-footer",
+                        label: url,
+                    });
+                }
+            }
+        });
+
         window.addEventListener("paramsupdated", this.handleParamsUpdated);
     }
 
