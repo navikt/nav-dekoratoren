24f37516ee3ace6c69659f8bf5c8f635322cae74
Andreas Nordahl <andnorda@gmail.com>
2024-07-11 14:27:15 +0200

    fix build-storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,7 @@
 /// <reference lib="DOM" />
 import type { Preview } from "@storybook/html";
 import "decorator-client/src/main.css";
-import "decorator-client/src/views/breadcrumb";
+import "decorator-client/src/views/breadcrumbs";
 import "decorator-client/src/views/user-menu";
 import "decorator-client/src/views/dropdown-menu";
 import "decorator-client/src/views/language-selector";
