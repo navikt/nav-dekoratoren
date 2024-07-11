80ab56c9a19fae1718b8a03030c872cda409fb16
Andreas Nordahl <andnorda@gmail.com>
2024-07-11 10:38:16 +0200

    Stop using lenke-med-sporing in footer (#390)
    gammel html (lenkene simple footer):
    
    ```html
    <div class="simple-footer-module__footerLinkList">
        <lenke-med-sporing
            data-analytics-event-args='{"category":"dekorator-footer","action":"kontakt/https://www.ansatt.dev.nav.no/no/nav-og-samfunn/om-nav/personvern-i-arbeids-og-velferdsetaten","label":"https://www.ansatt.dev.nav.no/no/nav-og-samfunn/om-nav/personvern-i-arbeids-og-velferdsetaten"}'
            href="https://www.ansatt.dev.nav.no/no/nav-og-samfunn/om-nav/personvern-i-arbeids-og-velferdsetaten"
            class="global-module__navds-link simple-footer-module__footerLink"
        >
            Personvern og informasjonskapsler </lenke-med-sporing
        ><lenke-med-sporing
            data-analytics-event-args='{"category":"dekorator-footer","action":"kontakt/https://www.ansatt.dev.nav.no/svindel","label":"https://www.ansatt.dev.nav.no/svindel"}'
            href="https://www.ansatt.dev.nav.no/svindel"
            class="global-module__navds-link simple-footer-module__footerLink"
        >
            Svindelforsøk og sikkerhet </lenke-med-sporing
        ><lenke-med-sporing
            data-analytics-event-args='{"category":"dekorator-footer","action":"kontakt/https://www.ansatt.dev.nav.no/tilgjengelighet","label":"https://www.ansatt.dev.nav.no/tilgjengelighet"}'
            href="https://www.ansatt.dev.nav.no/tilgjengelighet"
            class="global-module__navds-link simple-footer-module__footerLink"
        >
            Tilgjengelighet
        </lenke-med-sporing>
    </div>
    ```
    
    ny html:
    
    ```html
    <div class="simple-footer-module__footerLinkList">
        <a
            href="https://www.ansatt.dev.nav.no/no/nav-og-samfunn/om-nav/personvern-i-arbeids-og-velferdsetaten"
            class="global-module__navds-link simple-footer-module__footerLink"
            >Personvern og informasjonskapsler</a
        ><a
            href="https://www.ansatt.dev.nav.no/svindel"
            class="global-module__navds-link simple-footer-module__footerLink"
            >Svindelforsøk og sikkerhet</a
        ><a
            href="https://www.ansatt.dev.nav.no/tilgjengelighet"
            class="global-module__navds-link simple-footer-module__footerLink"
            >Tilgjengelighet</a
        >
    </div>
    
    ```

--- packages/server/src/views/footer/simple-footer.ts
+++ packages/server/src/views/footer/simple-footer.ts
@@ -3,30 +3,27 @@ import cls from "decorator-client/src/styles/simple-footer.module.css";
 import utilCls from "decorator-client/src/styles/utilities.module.css";
 import html from "decorator-shared/html";
 import { Features, Link } from "decorator-shared/types";
-import { LenkeMedSporing } from "decorator-shared/views/lenke-med-sporing-helpers";
 import i18n from "../../i18n";
 import { ScreenshareButton } from "./screenshare-button";
 
-export type SimpleFooterProps = {
+export const SimpleFooter = ({
+    links,
+    features,
+}: {
     links: Link[];
     features: Features;
-};
-
-export const SimpleFooter = ({ links, features }: SimpleFooterProps) => html`
+}) => html`
     <footer class="${cls.simpleFooter}">
         <div class="${cls.simpleFooterContent} ${utilCls.contentContainer}">
             <div class="${cls.footerLinkList}">
-                ${links.map(({ url, content }) =>
-                    LenkeMedSporing({
-                        href: url,
-                        children: content,
-                        className: `${globalCls["navds-link"]} ${cls.footerLink}`,
-                        analyticsEventArgs: {
-                            category: "dekorator-footer",
-                            action: `kontakt/${url}`,
-                            label: url,
-                        },
-                    }),
+                ${links.map(
+                    ({ url, content }) => html`
+                        <a
+                            href="${url}"
+                            class="${globalCls["navds-link"]} ${cls.footerLink}"
+                            >${content}</a
+                        >
+                    `,
                 )}
             </div>
             ${features["dekoratoren.skjermdeling"] &&
