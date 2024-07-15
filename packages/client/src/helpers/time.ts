f54b3a8e446d900ce1a901e8a49d0bd8fde3dd9d
Andreas Nordahl <andnorda@gmail.com>
2024-07-15 08:51:52 +0200

    delete unused time code

--- packages/client/src/helpers/time.ts
+++ packages/client/src/helpers/time.ts
@@ -1,20 +1,6 @@
-export const nowISOString = () => {
-    return new Date().toISOString();
-};
-
 export function addSecondsFromNow(seconds: number) {
     return new Date(Date.now() + seconds * 1000).toISOString();
 }
-
-export const addOneHourFromNow = () => {
-    const secondsInAnHour = 60 * 60;
-    return addSecondsFromNow(secondsInAnHour);
-};
-
-export const addSixHoursFromNow = () => {
-    const secondsInSixHours = 6 * 60 * 60;
-    return addSecondsFromNow(secondsInSixHours);
-};
 
 export const getSecondsRemaining = (futureDate: string) => {
     if (!futureDate) {
