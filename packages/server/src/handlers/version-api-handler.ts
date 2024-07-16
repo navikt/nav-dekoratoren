7c75313ea0dd800ebad422164256975e0420cb67
Anders Nomerstad <anders@nomerstad.com>
2024-07-16 15:40:16 +0200

    Watch configmap changes

--- packages/server/src/handlers/version-api-handler.ts
+++ packages/server/src/handlers/version-api-handler.ts
@@ -1,21 +1,35 @@
+import { watch } from "fs";
 import { env } from "../env/server";
 import { Handler } from "hono";
 
-const getVersionAuthority = async () => {
-    try {
-        const file = await Bun.file(
-            `${process.cwd()}/version-authority/version-authority.json`,
-        ).json();
+const filename = `${process.cwd()}/version-authority/version-authority.json`;
 
+const getAuthoritativeVersion = async () => {
+    try {
+        const file = await Bun.file(filename).json();
         return file.AUTHORITATIVE_VERSION_ID;
     } catch (e) {
         console.log("Error: ", e);
-        return "Not found";
+        return null;
     }
 };
 
-export const versionApiHandler: Handler = async ({ json }) =>
-    json({
-        localVersion: env.VERSION_ID,
-        authoritativeVersion: await getVersionAuthority(),
-    });
+const watcher = watch(filename, (event, filename) => {
+    console.log(`Watcher event for ${filename} - ${event}`);
+    const currentAuthoritativeVersion = getAuthoritativeVersion();
+    if (currentAuthoritativeVersion) {
+        versionData.authoritativeVersion = currentAuthoritativeVersion;
+    }
+});
+
+process.on("SIGINT", () => {
+    console.log(`Closing watcher for ${filename}`);
+    watcher.close();
+});
+
+const versionData = {
+    localVersion: env.VERSION_ID,
+    authoritativeVersion: await getAuthoritativeVersion(),
+};
+
+export const versionApiHandler: Handler = async ({ json }) => json(versionData);
