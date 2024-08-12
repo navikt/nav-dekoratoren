3310a79b4bfee2a4a219f34003af3a8005168ea6
Per Olav Mariussen <35491554+PerOlavM@users.noreply.github.com>
2024-08-12 12:17:04 +0200

    Legger tilbakemeldinger i en html-section med aria-label (#428)
    Gir bedre brukeropplevelse med skjermleser

--- packages/server/src/views/feedback.ts
+++ packages/server/src/views/feedback.ts
@@ -8,38 +8,40 @@ import { Button } from "./components/button";
 
 export const Feedback = ({ contactUrl }: { contactUrl: string }) => html`
     <d-feedback class="${cls.feedback}">
-        <div class="${cls.feedbackContent}">
-            <h2
-                class="${clsx(
-                    aksel["navds-body-short"],
-                    aksel["navds-body-short--large"],
-                )}"
-            >
-                ${i18n("did_you_find")}
-            </h2>
-            <div class="${cls.buttonWrapper}">
-                ${Button({
-                    content: i18n("yes"),
-                    variant: "secondary",
-                    attributes: { ["data-svar"]: "ja" },
-                })}
-                ${Button({
-                    content: i18n("no"),
-                    variant: "secondary",
-                    attributes: { ["data-svar"]: "nei" },
-                })}
+        <section aria-label="${i18n("feedback")} ${i18n("did_you_find")}"
+            <div class="${cls.feedbackContent}">
+                <h2
+                    class="${clsx(
+                        aksel["navds-body-short"],
+                        aksel["navds-body-short--large"],
+                    )}"
+                >
+                    ${i18n("did_you_find")}
+                </h2>
+                <div class="${cls.buttonWrapper}">
+                    ${Button({
+                        content: i18n("yes"),
+                        variant: "secondary",
+                        attributes: { ["data-svar"]: "ja" },
+                    })}
+                    ${Button({
+                        content: i18n("no"),
+                        variant: "secondary",
+                        attributes: { ["data-svar"]: "nei" },
+                    })}
+                </div>
             </div>
-        </div>
-        <div class="${clsx(cls.feedbackSuccess, utils.hidden)}">
-            <h2 class="${clsx(aksel["navds-heading"])}">
-                ${i18n("send_undersokelse_takk")}
-            </h2>
-            <div class="${aksel["navds-body-long"]}">
-                ${i18n("hensikt_med_tilbakemelding")}
+            <div class="${clsx(cls.feedbackSuccess, utils.hidden)}">
+                <h2 class="${clsx(aksel["navds-heading"])}">
+                    ${i18n("send_undersokelse_takk")}
+                </h2>
+                <div class="${aksel["navds-body-long"]}">
+                    ${i18n("hensikt_med_tilbakemelding")}
+                </div>
+                <a class="${aksel["navds-link"]}" href="${contactUrl}">
+                    ${i18n("hensikt_med_tilbakemelding_lenke")}
+                </a>
             </div>
-            <a class="${aksel["navds-link"]}" href="${contactUrl}">
-                ${i18n("hensikt_med_tilbakemelding_lenke")}
-            </a>
-        </div>
+        </section>
     </d-feedback>
 `;
