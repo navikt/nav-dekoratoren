c978db48b92c9aabf29867488a4f18eb28e3ac1e
Anders Nomerstad <anders@nomerstad.com>
2024-09-02 15:09:56 +0200

    Fjerner styles-felt fra /ssr response

--- packages/server/src/handlers/ssr-api.ts
+++ packages/server/src/handlers/ssr-api.ts
@@ -3,7 +3,6 @@ import { getFeatures } from "../unleash";
 import { HeaderTemplate } from "../views/header/header";
 import { FooterTemplate } from "../views/footer/footer";
 import { ScriptsTemplate } from "../views/scripts";
-import { StylesTemplate } from "../views/styles";
 import { HeadAssetsTemplate } from "../head";
 import { Handler } from "hono";
 import { env } from "../env/server";
@@ -12,7 +11,6 @@ type SsrPayload = {
     header: string;
     footer: string;
     scripts: string;
-    styles: string;
     headAssets: string;
     versionId: string;
 };
@@ -36,7 +34,6 @@ export const ssrApiHandler: Handler = async ({ req, json }) => {
             })
         ).render(params),
         scripts: ScriptsTemplate({ features, params }).render(params),
-        styles: StylesTemplate().render(),
         headAssets: HeadAssetsTemplate().render(),
         versionId: env.VERSION_ID,
     } satisfies SsrPayload);
