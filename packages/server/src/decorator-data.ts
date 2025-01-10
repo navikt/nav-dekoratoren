232a5546164439e00cb3c3712d5b6fdf82054973
Terje Karlsen <terjeofnorway@gmail.com>
2025-01-10 10:43:26 +0100

    Legger til liste over tillatte cookies for decoratorData

--- packages/server/src/decorator-data.ts
+++ packages/server/src/decorator-data.ts
@@ -1,3 +1,12 @@
+type StorageType = "cookie" | "localstorage" | "sessionstorage";
+
+export type StorageConfig = {
+    name: string;
+    type: StorageType[];
+    service: string;
+    description: string;
+    optional: boolean;
+};
 import {
     AppState,
     ClientTexts,
@@ -22,8 +31,148 @@ export type DecoratorDataProps = {
     headAssets?: HtmlElementProps[];
 };
 
-const getAllowedStorage = (): string[] => {
-    return [];
+const storageDictionary: Set<StorageConfig> = new Set([
+    {
+        name: "BIGipServerpool_pr_gcp_navno_lb_https",
+        type: ["cookie"],
+        service: "BigIP",
+        description:
+            "Brukes for effektiv kommunikasjon mellom nettleseren og nav.no.",
+        optional: false,
+    },
+    {
+        name: "com.grafana.faro*",
+        type: ["sessionstorage"],
+        service: "Grafana",
+        description:
+            "Overvåker teknisk status på nav.no og relaterte tjenester.",
+        optional: false,
+    },
+    {
+        name: "decorator-",
+        type: ["cookie"],
+        service: "Dekoratøren",
+        description: "Husker brukerens kontekst og valgt språk.",
+        optional: false,
+    },
+    {
+        name: "nav-chatbot",
+        type: ["cookie"],
+        service: "Nav chatbot",
+        description:
+            "Husker brukerens chatbot-samtale for å fortsette på tvers av sider.",
+        optional: false,
+    },
+    {
+        name: "__next_scroll",
+        type: ["sessionstorage"],
+        service: "Nav.no (Next)",
+        description: "Husker rulleposisjon mellom sider.",
+        optional: false,
+    },
+    {
+        name: "psCurrentState",
+        type: ["cookie"],
+        service: "Vergic",
+        description: "Deling av skjerm med veileder.",
+        optional: false,
+    },
+    {
+        name: "selvbetjening-idtoken",
+        type: ["cookie"],
+        service: "Innlogging",
+        description: "Husker innlogging på nav.no.",
+        optional: false,
+    },
+    {
+        name: "sso-nav.no",
+        type: ["cookie"],
+        service: "Innlogging",
+        description: "Husker innlogging på nav.no.",
+        optional: false,
+    },
+    {
+        name: "vngage.",
+        type: ["cookie", "sessionstorage", "localstorage"],
+        service: "Vergic",
+        description:
+            "Deling av skjerm med veileder, kun for autoriserte personer.",
+        optional: false,
+    },
+    {
+        name: "navno-consent-*",
+        type: ["cookie", "sessionstorage", "localstorage"],
+        service: "Dekoratøren",
+        description: "Husker brukerens samtykke for cookies.",
+        optional: false,
+    },
+    {
+        name: "_hjSession*",
+        type: ["cookie"],
+        service: "HotJar",
+        description: "Husker invitasjoner til brukerundersøkelser.",
+        optional: true,
+    },
+    {
+        name: "_tajs03412",
+        type: ["sessionstorage"],
+        service: "Task Analytics",
+        description: "Husker hvilke undersøkelser brukeren har takket ja til.",
+        optional: true,
+    },
+    {
+        name: "AMP_*",
+        type: ["localstorage"],
+        service: "Amplitude",
+        description: "Brukes til anonym statistikk og analyse av nav.no.",
+        optional: true,
+    },
+    {
+        name: "AMP_*",
+        type: ["cookie"],
+        service: "Amplitude",
+        description: "Brukes til anonym statistikk og analyse av nav.no.",
+        optional: true,
+    },
+    {
+        name: "ta-dekoratoren-*",
+        type: ["cookie"],
+        service: "Task Analytics",
+        description:
+            "Husker om en bruker har sett en invitasjon til undersøkelse.",
+        optional: true,
+    },
+    {
+        name: "usertest-*",
+        type: ["cookie"],
+        service: "Nav.no (Next)",
+        description:
+            "Husker hvilken variasjon av brukerundersøkelse som vises til brukeren.",
+        optional: true,
+    },
+]);
+
+export type PublicStorage = Pick<
+    StorageConfig,
+    "name" | "optional" | "type"
+> & {
+    type: StorageType;
+};
+
+const buildAllowedStorage = () => {
+    const allAllowedStorage: PublicStorage[] = Array.from(
+        storageDictionary,
+    ).reduce((all, item) => {
+        const subList = item.type.map((type) => ({
+            name: item.name,
+            optional: item.optional,
+            type,
+        })) as PublicStorage[];
+
+        return all.concat(subList);
+    }, [] as PublicStorage[]);
+
+    return allAllowedStorage;
 };
 
 export const buildDecoratorData = ({
@@ -54,5 +203,5 @@ export const buildDecoratorData = ({
     features,
     env: clientEnv,
     headAssets,
-    allowedStorage: getAllowedStorage(),
+    allowedStorage: buildAllowedStorage(),
 });
