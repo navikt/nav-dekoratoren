c9388965350ae06b51a7da3520a21bc048a57c20
Tale Prestmo <tale.prestmo@gmail.com>
2025-05-12 14:38:57 +0200

    oppdater tekst på consent banner

--- packages/server/src/views/consent-banner.ts
+++ packages/server/src/views/consent-banner.ts
@@ -26,33 +26,35 @@ export const ConsentBanner = () => html`
                 })}
             </div>
             <div class="${cls.content}">
-                <div class="${cls.column}">
-                    <h2
-                        id="consent_banner_title"
-                        class="${cls.title}"
-                        tabindex="-1"
-                    >
-                        ${i18n("consent_banner_title")}
-                    </h2>
-                    <div class="${cls.text}">
-                        ${i18n("consent_banner_text")}
-                    </div>
-                    <div class="${cls.buttonContainer}">
-                        ${Button({
-                            content: i18n("consent_banner_consent_all"),
-                            variant: "primary",
-                            attributes: { ["data-name"]: "consent-banner-all" },
-                            className: cls.button,
-                        })}
-                        ${Button({
-                            content: i18n("consent_banner_refuse_optional"),
-                            variant: "primary",
-                            attributes: {
-                                ["data-name"]: "consent-banner-refuse-optional",
-                            },
-                            className: cls.button,
-                        })}
-                    </div>
+                <h2
+                    id="consent_banner_title"
+                    class="${cls.title}"
+                    tabindex="-1"
+                >
+                    ${i18n("consent_banner_title")}
+                </h2>
+                <p class="${cls.text}">${i18n("consent_banner_text")}</p>
+                <div class="${cls.buttonContainer}">
+                    ${Button({
+                        content: i18n("consent_banner_consent_all"),
+                        variant: "primary",
+                        attributes: { ["data-name"]: "consent-banner-all" },
+                        className: cls.button,
+                    })}
+                    ${Button({
+                        content: i18n("consent_banner_refuse_optional"),
+                        variant: "primary",
+                        attributes: {
+                            ["data-name"]: "consent-banner-refuse-optional",
+                        },
+                        className: cls.button,
+                    })}
+                </div>
+                <p class="${cls.text}">
+                    ${i18n("consent_banner_change_consent")}
+                </p>
+                <div class="${cls.text}">
+                    ${i18n("consent_banner_additional_cookies_info")}
                 </div>
             </div>
         </section>
