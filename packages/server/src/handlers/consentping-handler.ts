d461821f3ab591bc29a58a7c43da4cf08bdd3e6a
Terje Karlsen <terje@terjeofnorway.no>
2026-02-13 06:40:52 +0100

    Legger til User-Agent i consent-ping

--- packages/server/src/handlers/consentping-handler.ts
+++ packages/server/src/handlers/consentping-handler.ts
@@ -35,6 +35,8 @@ export const consentpingHandler: Handler = async ({ req, json }) => {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "User-Agent":
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
             },
             body: JSON.stringify(umamiEvent),
         });
