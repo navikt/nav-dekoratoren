7ea61e110c0ab1cf7a25eaaa5bba4e5e9d083577
Dave Honneffer <david.honneffer@nav.no>
2026-09-10 10:24:10 +0200

    Handle playwright formally

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,9 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: process.env.CI ? "dot" : [["list"], ["html", { open: "never" }]],
+    reporter: process.env.CI
+        ? [["dot"], ["html", { open: "never" }]]
+        : [["list"], ["html", { open: "never" }]],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
